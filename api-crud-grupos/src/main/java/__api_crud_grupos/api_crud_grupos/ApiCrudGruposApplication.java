74b6f3cc734a72404f92df4e845669a62a17c356
Elderes <abraao.borges@academico.ifpb.edu.br>
2025-04-09 09:51:29 -0300

    commit inicial

---
+++ api-crud-grupos/src/main/java/__api_crud_grupos/api_crud_grupos/ApiCrudGruposApplication.java
@@ -0,0 +1,13 @@
+package __api_crud_grupos.api_crud_grupos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudGruposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudGruposApplication.class, args);
+	}
+
+}
---
+++ api-crud-grupos/src/test/java/__api_crud_grupos/api_crud_grupos/ApiCrudGruposApplicationTests.java
@@ -0,0 +1,13 @@
+package __api_crud_grupos.api_crud_grupos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudGruposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
