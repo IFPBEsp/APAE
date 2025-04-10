24b97f35781cca229b1faef017857ab8cda94cf5
dinizhugo <hugodiniztech01@gmail.com>
2025-04-10 12:16:09 -0300

    chore[]: Criação da estruturação dos pacotes.

---
+++ src/main/java/br/org/apae/laudos_medicos/LaudosMedicosApplication.java
@@ -0,0 +1,13 @@
+package br.org.apae.laudos_medicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaudosMedicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaudosMedicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/org/apae/laudos_medicos/LaudosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.apae.laudos_medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaudosMedicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
