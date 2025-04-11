b553e863c548daa785f68114526eabc3a7b641cb
Elderes <abraao.borges@academico.ifpb.edu.br>
2025-04-11 12:37:46 -0300

    feat: método de adicionar paciente a grupo

--- api-crud-de-grupos/src/main/java/br/org/apae/api_crud_de_grupos/service/GruposSerivce.java
+++ api-crud-de-grupos/src/main/java/br/org/apae/api_crud_de_grupos/service/GruposSerivce.java
@@ -1,5 +1,18 @@
 package br.org.apae.api_crud_de_grupos.service;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import br.org.apae.api_crud_de_grupos.model.Grupo;
+import br.org.apae.api_crud_de_grupos.model.Paciente;
+
+@Service
 public class GruposSerivce {
+    public void inserirPaciente(Paciente p, Grupo g) {
+        ArrayList<Paciente> novaLista = g.getListaDeParticipantes();
+        novaLista.add(p);
+        g.setListaDeParticipantes(novaLista);
+    }
 
 }
