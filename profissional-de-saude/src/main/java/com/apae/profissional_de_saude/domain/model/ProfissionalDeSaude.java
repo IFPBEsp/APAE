1117ddd656d7b278da9ee72cecf533fca1de5f88
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-13 15:43:23 -0300

    feat[domain]: Implementação do model ProfissionalDeSaude

--- profissional-de-saude/src/main/java/com/apae/profissional_de_saude/domain/model/ProfissionalDeSaude.java
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/domain/model/ProfissionalDeSaude.java
@@ -1,5 +1,20 @@
 package com.apae.profissional_de_saude.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class ProfissionalDeSaude {
-    // TODO -> implementar a entidade e suas regras
+    private Long id;
+    private String nome;
+    private String especialidade;
+    private String contato;
+
+    private List<Long> relatorioIds;
+    private List<Long> grupoIds;
 }
