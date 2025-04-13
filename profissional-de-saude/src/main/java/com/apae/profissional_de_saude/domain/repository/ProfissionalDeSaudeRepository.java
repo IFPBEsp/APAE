2996386be65017ce803bbe91e9ad8a48caa46bf2
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-13 15:51:21 -0300

    feat[repository]: Definição de contrato da interface repository para fazer o CRUD

--- profissional-de-saude/src/main/java/com/apae/profissional_de_saude/domain/repository/ProfissionalDeSaudeRepository.java
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/domain/repository/ProfissionalDeSaudeRepository.java
@@ -1,8 +1,16 @@
 package com.apae.profissional_de_saude.domain.repository;
 
+import com.apae.profissional_de_saude.domain.model.ProfissionalDeSaude;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProfissionalDeSaudeRepository {
-    // TODO -> implementar os contratos da interface repository
+    List<ProfissionalDeSaude> findAll();
+    Optional<ProfissionalDeSaude> findById(Long id);
+    void save(ProfissionalDeSaude profissionalDeSaude);
+    void update(ProfissionalDeSaude profissionalDeSaude);
+    void delete(Long id);
 }
--- profissional-de-saude/src/main/java/com/apae/profissional_de_saude/infrastructure/repository/JdbcProfissionalSaudeRepository.java
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/infrastructure/repository/JdbcProfissionalSaudeRepository.java
@@ -1,7 +1,37 @@
 package com.apae.profissional_de_saude.infrastructure.repository;
 
+import com.apae.profissional_de_saude.domain.model.ProfissionalDeSaude;
 import com.apae.profissional_de_saude.domain.repository.ProfissionalDeSaudeRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class JdbcProfissionalSaudeRepository implements ProfissionalDeSaudeRepository {
-    // TODO -> implementar os métodos dos contratos da interface
+    @Override
+    public List<ProfissionalDeSaude> findAll() {
+        // TODO -> implementar método
+        return List.of();
+    }
+
+    @Override
+    public Optional<ProfissionalDeSaude> findById(Long id) {
+        // TODO -> implementar método
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(ProfissionalDeSaude profissionalDeSaude) {
+        // TODO -> implementar método
+    }
+
+    @Override
+    public void update(ProfissionalDeSaude profissionalDeSaude) {
+        // TODO -> implementar método
+    }
+
+    @Override
+    public void delete(Long id) {
+        // TODO -> implementar método
+    }
+
 }
