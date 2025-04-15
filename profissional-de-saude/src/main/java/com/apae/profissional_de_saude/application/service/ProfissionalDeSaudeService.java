39d39cf00ce70fa9fbe06e12e65f12d7c49d8364
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-15 15:16:58 -0300

    feat[service]: adicionado o CRUD do service

--- profissional-de-saude/src/main/java/com/apae/profissional_de_saude/application/service/ProfissionalDeSaudeService.java
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/application/service/ProfissionalDeSaudeService.java
@@ -1,8 +1,37 @@
 package com.apae.profissional_de_saude.application.service;
 
+import com.apae.profissional_de_saude.domain.model.ProfissionalDeSaude;
+import com.apae.profissional_de_saude.domain.repository.ProfissionalDeSaudeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfissionalDeSaudeService {
-    // TODO -> Implementar os serviços
+    private final ProfissionalDeSaudeRepository repository;
+
+    public ProfissionalDeSaudeService(ProfissionalDeSaudeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<ProfissionalDeSaude> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<ProfissionalDeSaude> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void salvar(ProfissionalDeSaude profissionalDeSaude) {
+        repository.save(profissionalDeSaude);
+    }
+
+    public void atualizar(ProfissionalDeSaude profissionalDeSaude) {
+        repository.update(profissionalDeSaude);
+    }
+
+    public void remover(Long id) {
+        repository.delete(id);
+    }
 }
