c242de0866db2b26641d0b9a2a05dbd0e917a64e
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-15 15:10:13 -0300

    feat[domain]: Remoção do lombok da camada domain/model

--- profissional-de-saude/src/main/java/com/apae/profissional_de_saude/domain/model/ProfissionalDeSaude.java
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/domain/model/ProfissionalDeSaude.java
@@ -1,14 +1,7 @@
 package com.apae.profissional_de_saude.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
-@Setter
 public class ProfissionalDeSaude {
     private Long id;
     private String nome;
@@ -17,4 +10,61 @@ public class ProfissionalDeSaude {
 
     private List<Long> relatorioIds;
     private List<Long> grupoIds;
+
+    public ProfissionalDeSaude(String contato, String especialidade, List<Long> grupoIds, Long id, String nome, List<Long> relatorioIds) {
+        this.contato = contato;
+        this.especialidade = especialidade;
+        this.grupoIds = grupoIds;
+        this.id = id;
+        this.nome = nome;
+        this.relatorioIds = relatorioIds;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public List<Long> getGrupoIds() {
+        return grupoIds;
+    }
+
+    public void setGrupoIds(List<Long> grupoIds) {
+        this.grupoIds = grupoIds;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Long> getRelatorioIds() {
+        return relatorioIds;
+    }
+
+    public void setRelatorioIds(List<Long> relatorioIds) {
+        this.relatorioIds = relatorioIds;
+    }
 }
