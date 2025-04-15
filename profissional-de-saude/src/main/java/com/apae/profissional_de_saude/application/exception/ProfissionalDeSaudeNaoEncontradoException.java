f1a62b7e547491dab41d2f8bedadd4a70c9d7f60
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-15 15:58:56 -0300

    feat[]: adicionado exceção personalizada ProfissionalDeSaudeNaoEncontradoException

---
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/application/exception/ProfissionalDeSaudeNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.apae.profissional_de_saude.application.exception;
+
+public class ProfissionalDeSaudeNaoEncontradoException extends RuntimeException {
+    public ProfissionalDeSaudeNaoEncontradoException(String message) {
+        super(message);
+    }
+}
