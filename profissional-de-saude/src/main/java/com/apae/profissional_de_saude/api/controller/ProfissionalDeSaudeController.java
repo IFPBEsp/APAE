c954be458211ee9701d5c36d5b21abd0f42736aa
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-15 15:57:02 -0300

    feat[controller]: remoção do try catch

--- profissional-de-saude/src/main/java/com/apae/profissional_de_saude/api/controller/ProfissionalDeSaudeController.java
+++ profissional-de-saude/src/main/java/com/apae/profissional_de_saude/api/controller/ProfissionalDeSaudeController.java
@@ -5,6 +5,7 @@ import com.apae.profissional_de_saude.domain.model.ProfissionalDeSaude;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +22,8 @@ public class ProfissionalDeSaudeController {
 
     @GetMapping
     public ResponseEntity<List<ProfissionalDeSaude>> listar() {
-        try {
-            List<ProfissionalDeSaude> profissionalDeSaudes = service.listarTodos();
+        List<ProfissionalDeSaude> profissionalDeSaudes = service.listarTodos();
 
-            return ResponseEntity.status(HttpStatus.OK).body(profissionalDeSaudes);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }
+        return ResponseEntity.status(HttpStatus.OK).body(profissionalDeSaudes);
     }
 }
