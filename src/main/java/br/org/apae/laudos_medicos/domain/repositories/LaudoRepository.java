6127b4840867c93f95a2ea9737625e97ad729040
dinizhugo <hugodiniztech01@gmail.com>
2025-04-15 22:51:35 -0300

    feat[repository]: Adicionando os métodos na interface LaudoRepository

--- src/main/java/br/org/apae/laudos_medicos/domain/repositories/LaudoRepository.java
+++ src/main/java/br/org/apae/laudos_medicos/domain/repositories/LaudoRepository.java
@@ -1,4 +1,13 @@
 package br.org.apae.laudos_medicos.domain.repositories;
 
+import br.org.apae.laudos_medicos.domain.entities.Laudo;
+
+import java.util.List;
+
 public interface LaudoRepository {
+    Laudo save(Laudo laudo);
+    Laudo findById(Long id);
+    List<Laudo> findAll();
+    void delete(Long id);
+    List<Laudo> findLaudosByPacientId(Long idPaciente);
 }
