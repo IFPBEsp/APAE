fbbdc29e36dcbffbf0d8619b2c27c5061a7e52de
ismaellima4 <ismael.github@gmail.com>
2025-04-17 18:48:07 -0300

    feat[service]: adicionado as implementações de restClient para o LaudoService

--- src/main/java/br/org/apae/laudos_medicos/application/services/LaudoServiceImpl.java
+++ src/main/java/br/org/apae/laudos_medicos/application/services/LaudoServiceImpl.java
@@ -4,6 +4,10 @@ package br.org.apae.laudos_medicos.application.services;
 import br.org.apae.laudos_medicos.application.dtos.requests.LaudoRequestDTO;
 import br.org.apae.laudos_medicos.application.exceptions.LaudoNaoEncontradoException;
 import br.org.apae.laudos_medicos.domain.entities.Laudo;
+import br.org.apae.laudos_medicos.infrastructure.clients.MedicoClient;
+import br.org.apae.laudos_medicos.infrastructure.clients.PacienteClient;
+import br.org.apae.laudos_medicos.infrastructure.clients.dtos.MedicoResponseDTO;
+import br.org.apae.laudos_medicos.infrastructure.clients.dtos.PacienteResponseDTO;
 import br.org.apae.laudos_medicos.infrastructure.persistences.LaudoRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,19 +18,23 @@ import java.util.List;
 public class LaudoServiceImpl implements LaudoService {
 
     @Autowired
-    private final LaudoRepositoryImpl laudoRepository;
+    private LaudoRepositoryImpl laudoRepository;
 
-    public LaudoServiceImpl(LaudoRepositoryImpl laudoRepository) {
-        this.laudoRepository = laudoRepository;
-    }
+    @Autowired
+    private PacienteClient pacienteClient;
+
+    @Autowired
+    private MedicoClient medicoClient;
 
     @Override
     public Laudo criarNovoLaudo(LaudoRequestDTO laudoRequestDTO) {
+        PacienteResponseDTO pacienteResponseDTO = this.pacienteClient.buscarPacientePorId(laudoRequestDTO.idPaciente());
+        MedicoResponseDTO medicoResponseDTO = this.medicoClient.buscarMedicoPorId(laudoRequestDTO.idMedico());
         Laudo laudo = new Laudo();
         laudo.setDataEmissao(laudoRequestDTO.dataEmissao());
         laudo.setDescricao(laudoRequestDTO.descricao());
-        laudo.setIdMedico(laudoRequestDTO.idMedico());
-        laudo.setIdPaciente(laudoRequestDTO.idPaciente());
+        laudo.setIdMedico(medicoResponseDTO.id());
+        laudo.setIdPaciente(pacienteResponseDTO.id());
         return this.laudoRepository.save(laudo);
     }
 
@@ -48,8 +56,14 @@ public class LaudoServiceImpl implements LaudoService {
     @Override
     public Laudo atualizarLaudo(Long id, LaudoRequestDTO laudoRequestDTO) {
         Laudo persitLaudo = this.buscarLaudoPorId(id);
-        if (laudoRequestDTO.idMedico() != null) persitLaudo.setIdMedico(laudoRequestDTO.idMedico());
-        if (laudoRequestDTO.idPaciente() != null) persitLaudo.setIdPaciente(laudoRequestDTO.idPaciente());
+        if (laudoRequestDTO.idPaciente() != null && !laudoRequestDTO.idPaciente().equals(persitLaudo.getIdPaciente())) {
+            PacienteResponseDTO pacienteResponseDTO = this.pacienteClient.buscarPacientePorId(laudoRequestDTO.idPaciente());
+            persitLaudo.setIdPaciente(pacienteResponseDTO.id());
+        }
+        if (laudoRequestDTO.idMedico() != null && !laudoRequestDTO.idMedico().equals(persitLaudo.getIdMedico())) {
+            MedicoResponseDTO medicoResponseDTO = this.medicoClient.buscarMedicoPorId(laudoRequestDTO.idMedico());
+            persitLaudo.setIdPaciente(medicoResponseDTO.id());
+        }
         if (laudoRequestDTO.descricao() != null) persitLaudo.setDescricao(laudoRequestDTO.descricao());
         if (laudoRequestDTO.dataEmissao() != null) persitLaudo.setDataEmissao(laudoRequestDTO.dataEmissao());
         return this.laudoRepository.save(persitLaudo);
