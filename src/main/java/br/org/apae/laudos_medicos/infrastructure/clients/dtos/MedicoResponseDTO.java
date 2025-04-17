267524569c76ed543d1b9d2c7eb51b9e54dffd12
ismaellima4 <ismael.github@gmail.com>
2025-04-17 18:53:18 -0300

    feat[dto]: adicionando dtos para os restClients de medico e paciente

---
+++ src/main/java/br/org/apae/laudos_medicos/infrastructure/clients/dtos/MedicoResponseDTO.java
@@ -0,0 +1,4 @@
+package br.org.apae.laudos_medicos.infrastructure.clients.dtos;
+
+public record MedicoResponseDTO(Long id, String nome) {
+}
---
+++ src/main/java/br/org/apae/laudos_medicos/infrastructure/clients/dtos/PacienteResponseDTO.java
@@ -0,0 +1,4 @@
+package br.org.apae.laudos_medicos.infrastructure.clients.dtos;
+
+public record PacienteResponseDTO(Long id, String nome) {
+}
