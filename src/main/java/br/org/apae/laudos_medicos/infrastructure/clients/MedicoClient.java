76672816d05c8fc06313780881b956c7e68c3e43
ismaellima4 <ismael.github@gmail.com>
2025-04-17 18:46:28 -0300

    feat[restClient]: adicionado restClient para medico e paciente

--- src/main/java/br/org/apae/laudos_medicos/infrastructure/clients/MedicoClient.java
+++ src/main/java/br/org/apae/laudos_medicos/infrastructure/clients/MedicoClient.java
@@ -1,4 +1,30 @@
 package br.org.apae.laudos_medicos.infrastructure.clients;
 
+import br.org.apae.laudos_medicos.infrastructure.clients.dtos.MedicoResponseDTO;
+import br.org.apae.laudos_medicos.infrastructure.clients.exceptions.IdMedicoInvalidoException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
 public class MedicoClient {
+
+    private final RestClient restClient;
+
+    public MedicoClient(RestClient.Builder restClient, @Value("${api.medicos.url}") String url) {
+        this.restClient = restClient
+                .baseUrl(url)
+                .build();
+    }
+
+    public MedicoResponseDTO buscarMedicoPorId(Long idMedico) {
+        return this.restClient.get()
+                .uri("/medicos/{id}", idMedico)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    throw new IdMedicoInvalidoException("Id do médico não existe ou não encontrado.");
+                })
+                .body(MedicoResponseDTO.class);
+    }
 }
--- src/main/java/br/org/apae/laudos_medicos/infrastructure/clients/PacienteClient.java
+++ src/main/java/br/org/apae/laudos_medicos/infrastructure/clients/PacienteClient.java
@@ -1,4 +1,31 @@
 package br.org.apae.laudos_medicos.infrastructure.clients;
 
+import br.org.apae.laudos_medicos.infrastructure.clients.dtos.MedicoResponseDTO;
+import br.org.apae.laudos_medicos.infrastructure.clients.dtos.PacienteResponseDTO;
+import br.org.apae.laudos_medicos.infrastructure.clients.exceptions.IdMedicoInvalidoException;
+import br.org.apae.laudos_medicos.infrastructure.clients.exceptions.IdPacienteInvalidoException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
+
 public class PacienteClient {
+
+    private final RestClient restClient;
+
+    public PacienteClient(RestClient.Builder restClient, @Value("${api.medicos.url}") String url) {
+        this.restClient = restClient
+                .baseUrl(url)
+                .build();
+    }
+
+
+    public PacienteResponseDTO buscarPacientePorId(Long idPaciente) {
+        return this.restClient.get()
+                .uri("/pacientes/{id}", idPaciente)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    throw new IdPacienteInvalidoException("Id do paciente não existe ou não encontrado.");
+                })
+                .body(PacienteResponseDTO.class);
+    }
 }
