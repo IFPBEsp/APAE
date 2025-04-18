d4735b1b5686dc6fe2ff758f2e5806465958714e
isnael07 <isnaellima24@gmail.com>
2025-04-18 11:47:40 -0300

    chore: inicializando estrutura de diretórios

---
+++ api_crud_medicos/src/main/java/br/org/apae/api_crud_medicos/ApiCrudMedicosApplication.java
@@ -0,0 +1,13 @@
+package br.org.apae.api_crud_medicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudMedicosApplication.class, args);
+	}
+
+}
---
+++ api_crud_medicos/src/test/java/br/org/apae/api_crud_medicos/ApiCrudMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.apae.api_crud_medicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
