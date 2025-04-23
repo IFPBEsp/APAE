eba639eca8998998f1ce5ca4b08e5fe5efff3650
Elderes <abraao.borges@academico.ifpb.edu.br>
2025-04-23 06:51:54 -0300

    refactor[Paciente]: concordância com nova modelagem do banco de dados.

--- api-crud-pacientes/src/main/java/br/org/apae/api_crud_pacientes/model/Paciente.java
+++ api-crud-pacientes/src/main/java/br/org/apae/api_crud_pacientes/model/Paciente.java
@@ -2,6 +2,7 @@ package br.org.apae.api_crud_pacientes.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -13,29 +14,13 @@ public class Paciente {
     private UUID id;
 
     @Column(name = "nome_completo", nullable = false)
-    private String nomeCompleto;
-
-    private enum genero {
-        MASCULINO,
-        FEMININO,
-        OUTRO
-    };
+    private String nome_completo;
 
     @Column()
-    private String contato;
-
-    @Column()
-    private String endereco;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "ficha_medica_id", referencedColumnName = "id")
-    private FichaMedica fichaMedica;
+    private List<String> contatos;
 
     @Column(name = "data_nascimento", nullable = false)
-    private LocalDate dataNascimento;
-
-    @Column(nullable = false, unique = true)
-    private String cpf;
+    private LocalDate data_nascimento;
 
     public UUID getId() {
         return id;
@@ -45,52 +30,28 @@ public class Paciente {
         this.id = id;
     }
 
-    public String getNomeCompleto() {
-        return nomeCompleto;
+    public String getNome_completo() {
+        return nome_completo;
     }
 
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
     }
 
-    public String getContato() {
-        return contato;
+    public List<String> getContatos() {
+        return contatos;
     }
 
-    public void setContato(String contato) {
-        this.contato = contato;
+    public void setContatos(List<String> contatos) {
+        this.contatos = contatos;
     }
 
-    public String getEndereco() {
-        return endereco;
+    public LocalDate getData_nascimento() {
+        return data_nascimento;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public FichaMedica getFichaMedica() {
-        return fichaMedica;
-    }
-
-    public void setFichaMedica(FichaMedica fichaMedica) {
-        this.fichaMedica = fichaMedica;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public void setData_nascimento(LocalDate data_nascimento) {
+        this.data_nascimento = data_nascimento;
     }
 
     @Override
@@ -98,12 +59,9 @@ public class Paciente {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((nomeCompleto == null) ? 0 : nomeCompleto.hashCode());
-        result = prime * result + ((contato == null) ? 0 : contato.hashCode());
-        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
-        result = prime * result + ((fichaMedica == null) ? 0 : fichaMedica.hashCode());
-        result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
-        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((nome_completo == null) ? 0 : nome_completo.hashCode());
+        result = prime * result + ((contatos == null) ? 0 : contatos.hashCode());
+        result = prime * result + ((data_nascimento == null) ? 0 : data_nascimento.hashCode());
         return result;
     }
 
@@ -121,44 +79,28 @@ public class Paciente {
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (nomeCompleto == null) {
-            if (other.nomeCompleto != null)
+        if (nome_completo == null) {
+            if (other.nome_completo != null)
                 return false;
-        } else if (!nomeCompleto.equals(other.nomeCompleto))
+        } else if (!nome_completo.equals(other.nome_completo))
             return false;
-        if (contato == null) {
-            if (other.contato != null)
+        if (contatos == null) {
+            if (other.contatos != null)
                 return false;
-        } else if (!contato.equals(other.contato))
+        } else if (!contatos.equals(other.contatos))
             return false;
-        if (endereco == null) {
-            if (other.endereco != null)
+        if (data_nascimento == null) {
+            if (other.data_nascimento != null)
                 return false;
-        } else if (!endereco.equals(other.endereco))
-            return false;
-        if (fichaMedica == null) {
-            if (other.fichaMedica != null)
-                return false;
-        } else if (!fichaMedica.equals(other.fichaMedica))
-            return false;
-        if (dataNascimento == null) {
-            if (other.dataNascimento != null)
-                return false;
-        } else if (!dataNascimento.equals(other.dataNascimento))
-            return false;
-        if (cpf == null) {
-            if (other.cpf != null)
-                return false;
-        } else if (!cpf.equals(other.cpf))
+        } else if (!data_nascimento.equals(other.data_nascimento))
             return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Paciente [id=" + id + ", nomeCompleto=" + nomeCompleto + ", contato=" + contato + ", endereco="
-                + endereco + ", fichaMedica=" + fichaMedica + ", dataNascimento=" + dataNascimento + ", cpf=" + cpf
-                + "]";
+        return "Paciente [id=" + id + ", nome_completo=" + nome_completo + ", contatos=" + contatos
+                + ", data_nascimento=" + data_nascimento + "]";
     }
 
 }
