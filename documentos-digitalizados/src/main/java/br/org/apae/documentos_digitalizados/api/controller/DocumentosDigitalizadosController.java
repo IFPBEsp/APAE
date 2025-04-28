fc7a2dba9bbf2994c99d674f20636f606125a242
isnael07 <isnaellima24@gmail.com>
2025-04-28 17:49:10 -0300

    feat[controller] : Adicionando a interface de controller de documentos digitalizados

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosController.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosController.java
@@ -1,9 +1,57 @@
 package br.org.apae.documentos_digitalizados.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosRequestDTO;
+import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+import java.util.List;
+
 @RequestMapping("/pacientes")
-public class DocumentosDigitalizadosController {
+public interface DocumentosDigitalizadosController {
+
+
+    @GetMapping("/{id_paciente}/documentos")
+    ResponseEntity<List<DocumentosDigitalizadosResponseDTO>> listarDocumentosPaciente(
+            @PathVariable Long id_paciente);
+
+
+    @GetMapping("/{id_paciente}/laudos_medico/documentos")
+    ResponseEntity<List<DocumentosDigitalizadosResponseDTO>> listarLaudosMedicos(
+            @PathVariable Long id_paciente);
+
+
+    @PostMapping("/{id_paciente}/laudos_medico/documentos")
+    ResponseEntity<DocumentosDigitalizadosResponseDTO> uploadLaudoMedico(
+            @PathVariable Long id_paciente,
+            @RequestParam("file") MultipartFile file,
+            @RequestBody DocumentosDigitalizadosRequestDTO requestDTO);
+
+    @GetMapping("/{id_paciente}/laudos_medico/documentos/{id}")
+    ResponseEntity<DocumentosDigitalizadosResponseDTO> obterLaudoMedico(
+            @PathVariable Long id_paciente,
+            @PathVariable Long id);
+
+    @GetMapping("/{id_paciente}/documentos/{id}")
+    ResponseEntity<DocumentosDigitalizadosResponseDTO> obterDocumento(
+            @PathVariable Long id_paciente,
+            @PathVariable Long id);
+
+    @PostMapping("/{id_paciente}/documentos")
+    ResponseEntity<DocumentosDigitalizadosResponseDTO> uploadDocumento(
+            @PathVariable Long id_paciente,
+            @RequestParam("file") MultipartFile file,
+            @RequestBody DocumentosDigitalizadosRequestDTO requestDTO);
+
+    @PutMapping("/{id_paciente}/documentos/{id}")
+    ResponseEntity<DocumentosDigitalizadosResponseDTO> atualizarDocumento(
+            @PathVariable Long id_paciente,
+            @PathVariable Long id,
+            @RequestBody DocumentosDigitalizadosRequestDTO requestDTO);
+
+    @DeleteMapping("/{id_paciente}/documentos/{id}")
+    ResponseEntity<Void> removerDocumento(
+            @PathVariable Long id_paciente,
+            @PathVariable Long id);
 }
