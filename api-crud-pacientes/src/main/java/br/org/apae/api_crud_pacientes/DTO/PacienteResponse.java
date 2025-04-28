88f3cce6eec8f079b5a983d4a6f280bd399629fc
meduarda18 <mariaeduardac604@gmail.com>
2025-04-28 14:30:17 -0300

    feat: adição do PacienteResponse para resposta da API

---
+++ api-crud-pacientes/src/main/java/br/org/apae/api_crud_pacientes/DTO/PacienteResponse.java
@@ -0,0 +1,50 @@
+package br.org.apae.api_crud_pacientes.DTO;
+
+import br.org.apae.api_crud_pacientes.model.Paciente;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class PacienteResponse {
+
+    private UUID id;
+    private  String nome_completo;
+    private List<String> contatos;
+    private LocalDate data_nascimento;
+
+    public PacienteResponse(Paciente paciente) {
+        this.id = paciente.getId();
+        this.nome_completo = paciente.getNome_completo();
+        this.contatos = paciente.getContatos();
+        this.data_nascimento = paciente.getData_nascimento();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome_completo() {
+        return nome_completo;
+    }
+
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
+    }
+
+    public List<String> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<String> contatos) {
+        this.contatos = contatos;
+    }
+
+    public LocalDate getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(LocalDate data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+}
