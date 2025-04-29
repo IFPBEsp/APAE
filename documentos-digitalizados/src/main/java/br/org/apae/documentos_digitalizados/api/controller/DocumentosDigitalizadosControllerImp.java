3318459993ccef09fdc730a98ba38bb164925497
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-29 15:32:10 -0300

    chore[controller]: alteração nos end points

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosControllerImp.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosControllerImp.java
@@ -2,9 +2,9 @@ package br.org.apae.documentos_digitalizados.api.controller;
 
 import br.org.apae.documentos_digitalizados.application.service.DocumentosDigitalidadosService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
