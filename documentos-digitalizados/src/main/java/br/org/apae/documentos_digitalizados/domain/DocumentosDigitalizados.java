7b2c5d756780b0b9cc02be50d8d2ecaa4b06bef0
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-29 16:00:46 -0300

    chore[domain]: alteração na coluna pacientId

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/DocumentosDigitalizados.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/DocumentosDigitalizados.java
@@ -14,7 +14,7 @@ public class DocumentosDigitalizados {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private Long pacienteId;
 
     @Column(nullable = false, unique = true)
