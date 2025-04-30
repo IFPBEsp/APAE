2dfe52a13adc447a3214b83912ae71fbb680bc7c
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-30 11:39:38 -0300

    feat[application]: dto de request para criar um novo documento

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/dtos/DocumentosDigitalizadosRequestDTO.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/dtos/DocumentosDigitalizadosRequestDTO.java
@@ -1,4 +1,10 @@
 package br.org.apae.documentos_digitalizados.application.dtos;
 
-public record DocumentosDigitalizadosRequestDTO(Long pacienteId) {
+import br.org.apae.documentos_digitalizados.domain.TipoDeDocumento;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DocumentosDigitalizadosRequestDTO(@NotBlank(message = "ID do paciente é obrigatório!") Long pacienteId,
+                                                @NotNull(message = "Tipo do documento(pessoal, medico, escolar) é obrigatório!") TipoDeDocumento tipoDeDocumento,
+                                                @NotBlank(message = "Nome do documento é obrigatório!") String nomeDocumento) {
 }
