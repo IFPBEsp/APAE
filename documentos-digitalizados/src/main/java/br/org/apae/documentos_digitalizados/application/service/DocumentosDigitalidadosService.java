c1cca011c49a70330256611c04b7834a3eb278a4
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-30 11:42:41 -0300

    chore[service]: interface do service do documento digitalizado atualizado

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/DocumentosDigitalidadosService.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/DocumentosDigitalidadosService.java
@@ -2,13 +2,15 @@ package br.org.apae.documentos_digitalizados.application.service;
 
 import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosRequestDTO;
 import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosResponseDTO;
+import br.org.apae.documentos_digitalizados.domain.DocumentosDigitalizados;
 
 import java.util.List;
 
 public interface DocumentosDigitalidadosService {
-    DocumentosDigitalizadosResponseDTO criar(DocumentosDigitalizadosRequestDTO dto);
-    DocumentosDigitalizadosResponseDTO buscarPorId(Long id);
-    List<DocumentosDigitalizadosResponseDTO> listarTodos();
-    DocumentosDigitalizadosResponseDTO atualizar(Long id, DocumentosDigitalizadosRequestDTO dto);
+    void criar(DocumentosDigitalizadosRequestDTO dto);
+    DocumentosDigitalizados buscarPorId(Long id);
+    List<DocumentosDigitalizados> listarTodos();
+    void atualizar(Long id, DocumentosDigitalizadosRequestDTO dto);
     void excluir(Long id);
+    DocumentosDigitalizados buscarPorPaciente(Long id);
 }
