8b3dd0924f1465f2f13a3b8726066ea007c96259
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-30 16:09:35 -0300

    chore[controller]: adicionado a validação do dto

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosController.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosController.java
@@ -3,6 +3,7 @@ package br.org.apae.documentos_digitalizados.api.controller;
 import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosRequestDTO;
 import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosResponseDTO;
 import br.org.apae.documentos_digitalizados.domain.TipoDocumento;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import java.awt.print.Pageable;
 @RequestMapping("/documento")
 public interface DocumentosDigitalizadosController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    ResponseEntity<Void> salvarDocumento(@RequestPart("documento") DocumentosDigitalizadosRequestDTO dto,
+    ResponseEntity<Void> salvarDocumento(@RequestPart("documento") @Valid DocumentosDigitalizadosRequestDTO dto,
                                          @RequestPart("anexo") MultipartFile documento);
 
     @GetMapping
@@ -29,7 +30,7 @@ public interface DocumentosDigitalizadosController {
 
     @PutMapping(value = "/{nomeDoDocumento}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<Void> atualizarDocumento(@PathVariable String nomeDoDocumento,
-                                            @RequestPart("documento") DocumentosDigitalizadosRequestDTO dto,
+                                            @RequestPart("documento") @Valid DocumentosDigitalizadosRequestDTO dto,
                                             @RequestPart("anexo") MultipartFile documento);
 
     @DeleteMapping("/{nomeDoDocumento}")
