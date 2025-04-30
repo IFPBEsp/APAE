bb5077724e38dc2291a6b60a92dece55ab4de6a1
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-30 16:04:17 -0300

    chore[service]: mudança geral da interface e implementação do serviço do minIO

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageService.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageService.java
@@ -1,13 +1,11 @@
 package br.org.apae.documentos_digitalizados.application.service;
 
+import br.org.apae.documentos_digitalizados.domain.TipoDocumento;
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
-import java.util.UUID;
-
 public interface MinioStorageService {
-    void uploadLaudo(UUID documentId, MultipartFile file) throws Exception;
-    void uploadEncaminhamento(UUID documentId, MultipartFile file) throws Exception;
-    InputStream downloadLaudo(UUID documentId) throws Exception ;
-    InputStream downloadEncaminhamento(UUID documentId) throws Exception ;
+
+    void uploadDocumento(TipoDocumento tipo, String documentoNome, MultipartFile file);
+    Resource downloadDocumento(TipoDocumento tipo, String documentoNome);
 }
--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageServiceImp.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageServiceImp.java
@@ -1,15 +1,19 @@
 package br.org.apae.documentos_digitalizados.application.service;
 
+import br.org.apae.documentos_digitalizados.application.exception.DocumentoStorageException;
+import br.org.apae.documentos_digitalizados.domain.TipoDocumento;
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -17,53 +21,78 @@ public class MinioStorageServiceImp implements MinioStorageService {
 
     private final MinioClient minioClient;
 
-    @Value("${minio.buckets.laudos}")
-    private String bucketLaudos;
+    @Value("${minio.buckets.pessoal}")
+    private String bucketDocumentoPessoal;
 
-    @Value("${minio.buckets.encaminhamentos}")
-    private String bucketEncaminhamentos;
+    @Value("${minio.buckets.medico}")
+    private String bucketDocumentoMedico;
+
+    @Value("${minio.buckets.escolar}")
+    private String bucketDocumentoEscolar;
+
 
     @Override
-    public void uploadLaudo(UUID UUIDLaudo, MultipartFile file) throws Exception {
-        minioClient.putObject(
-                PutObjectArgs.builder()
-                        .bucket(bucketLaudos)
-                        .object(UUIDLaudo.toString())
-                        .stream(file.getInputStream(), file.getSize(), -1)
-                        .contentType(file.getContentType())
-                        .build()
-        );
+    public void uploadDocumento(TipoDocumento tipo, String documentoNome, MultipartFile file) {
+        validarArquivo(file);
+
+        String bucket = determinarBucket(tipo);
+
+        try (InputStream inputStream = file.getInputStream()) {
+            salvarDocumento(bucket, documentoNome, inputStream, file.getContentType(), file.getSize());
+        } catch (IOException e) {
+            throw new DocumentoStorageException("Falha ao ler arquivo\n" + e);
+        }
     }
 
-    @Override
-    public void uploadEncaminhamento(UUID UUIDEncaminhamento, MultipartFile file) throws Exception {
-        minioClient.putObject(
-                PutObjectArgs.builder()
-                        .bucket(bucketEncaminhamentos)
-                        .object(UUIDEncaminhamento.toString())
-                        .stream(file.getInputStream(), file.getSize(), -1)
-                        .contentType(file.getContentType())
-                        .build()
-        );
+    public Resource downloadDocumento(TipoDocumento tipo, String documentoNome) {
+        try {
+            InputStream stream = minioClient.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(determinarBucket(tipo))
+                            .object(documentoNome)
+                            .build()
+            );
+
+            return new InputStreamResource(stream) {
+                @Override
+                public String getFilename() {
+                    return documentoNome;
+                }
+            };
+        } catch (Exception e) {
+            throw new DocumentoStorageException("Erro ao baixar documento\n" + e);
+        }
     }
 
-    @Override
-    public InputStream downloadLaudo(UUID UUIDLaudo) throws Exception {
-        return minioClient.getObject(
-                GetObjectArgs.builder()
-                        .bucket(bucketLaudos)
-                        .object(UUIDLaudo.toString())
-                        .build()
-        );
+    private void validarArquivo(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new DocumentoStorageException("Arquivo não pode ser nulo ou vazio");
+        }
+
+        if (file.getContentType() == null) {
+            throw new DocumentoStorageException("Tipo de conteúdo não identificado");
+        }
     }
 
-    @Override
-    public InputStream downloadEncaminhamento(UUID UUIDEncaminhamento) throws Exception {
-        return minioClient.getObject(
-                GetObjectArgs.builder()
-                        .bucket(bucketEncaminhamentos)
-                        .object(UUIDEncaminhamento.toString())
-                        .build()
-        );
+    private String determinarBucket(TipoDocumento tipo) {
+        return switch (tipo) {
+            case PESSOAL -> bucketDocumentoPessoal;
+            case MEDICO -> bucketDocumentoMedico;
+            case ESCOLAR -> bucketDocumentoEscolar;
+        };
+    }
+
+    private void salvarDocumento(String bucket, String objectName, InputStream inputStream, String contentType, long size) {
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(objectName)
+                            .stream(inputStream, size, -1)
+                            .contentType(contentType)
+                            .build());
+        } catch (Exception e) {
+            throw new DocumentoStorageException("Falha no upload para MinIO\n" + e);
+        }
     }
 }
