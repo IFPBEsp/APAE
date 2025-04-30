d9b86efe60afc7ecd2a488d2549299ec8946256e
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-30 15:59:41 -0300

    feat[application]: adição de excessão personalizadas

---
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/exception/SalvarDocumentoException.java
@@ -0,0 +1,7 @@
+package br.org.apae.documentos_digitalizados.application.exception;
+
+public class SalvarDocumentoException extends RuntimeException {
+    public SalvarDocumentoException(String message) {
+        super(message);
+    }
+}
