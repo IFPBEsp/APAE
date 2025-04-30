fe568e97d7ce5aa8fe7c95f27fa2eacf3b23f621
lucasgomes14 <lucazmatehus14@gmail.com>
2025-04-30 15:59:03 -0300

    chore[application]: atualização do mapper

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/mapper/DocumentoDigitalizadosMapper.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/mapper/DocumentoDigitalizadosMapper.java
@@ -1,38 +1,24 @@
 package br.org.apae.documentos_digitalizados.application.mapper;
 
 import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosRequestDTO;
-import br.org.apae.documentos_digitalizados.application.dtos.DocumentosDigitalizadosResponseDTO;
+import br.org.apae.documentos_digitalizados.application.exception.ExtensaoArquivoException;
 import br.org.apae.documentos_digitalizados.domain.DocumentosDigitalizados;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Component
 public class DocumentoDigitalizadosMapper {
-    public List<DocumentosDigitalizadosResponseDTO> toDTO(List<DocumentosDigitalizados> documentosDigitalizados) {
-        return documentosDigitalizados.stream()
-            .map(documento -> new DocumentosDigitalizadosResponseDTO(
-                documento.getId(),
-                documento.getPacienteId(),
-                documento.getEncaminhamento(),
-                documento.getLaudoMedico()
-            )).collect(Collectors.toList()
-        );
-    }
 
-    public DocumentosDigitalizadosResponseDTO toDTO(DocumentosDigitalizados documentosDigitalizados) {
-        return new DocumentosDigitalizadosResponseDTO(
-                documentosDigitalizados.getId(),
-                documentosDigitalizados.getPacienteId(),
-                documentosDigitalizados.getEncaminhamento(),
-                documentosDigitalizados.getLaudoMedico()
-        );
-    }
-
-    public DocumentosDigitalizados toEntity(DocumentosDigitalizadosRequestDTO documentosDigitalizadosRequestDTO) {
+    public DocumentosDigitalizados toEntity(DocumentosDigitalizadosRequestDTO dto, MultipartFile arquivo) {
         DocumentosDigitalizados documento = new DocumentosDigitalizados();
-        documento.setPacienteId(documentosDigitalizadosRequestDTO.pacienteId());
+        documento.setPacienteId(dto.pacienteId());
+        documento.setTipoDocumento(dto.tipoDocumento());
+
+        String extensao = extrairExtensao(arquivo.getOriginalFilename());
+
+        documento.setDocumento(dto.nomeDocumento() + "-" + UUID.randomUUID() + extensao);
 
         return documento;
     }
@@ -43,5 +29,13 @@ public class DocumentoDigitalizadosMapper {
         documento.setPacienteId(documentosDigitalizadosRequestDTO.pacienteId());
 
         return documento;
+    }
+
+    private String extrairExtensao(String nomeArquivo) {
+        if (nomeArquivo != null && !nomeArquivo.isEmpty()) {
+            return nomeArquivo.substring(0, nomeArquivo.lastIndexOf("."));
+        }
+
+       throw new ExtensaoArquivoException("Arquivo não contém extensão!");
     }
 }
