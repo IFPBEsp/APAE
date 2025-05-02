9940ce05062710c1b94041290e5c928ca277c0a6
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-02 15:25:57 -0300

    chore[service]: mudança na interface do MinioStorageService

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageService.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageService.java
@@ -1,15 +1,16 @@
 package br.org.apae.documentos_digitalizados.application.service;
 
 import br.org.apae.documentos_digitalizados.domain.TipoDocumento;
+import br.org.apae.documentos_digitalizados.domain.TipoPaciente;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.List;
 
 public interface MinioStorageService {
-    void criarBucket(String bucketNome);
-    boolean existeBucket(String bucketNome);
-    String listarDocumentos(String bucketNome, String subBucket);
-    void uploadDocumento(TipoDocumento tipo, String documentoNome, MultipartFile file);
-    InputStream downloadDocumento(TipoDocumento tipo, String documentoNome);
-    void atualizarDocumento(String bucketNome, String documentoNome, MultipartFile file);
+    void criarBucket(String bucketNome, TipoPaciente tipoPaciente);
+    void uploadDocumento(String nomeBucket, TipoDocumento tipoDocumento, String documentoNome, MultipartFile file);
+    InputStream downloadDocumento(String bucketNome, String documentoCaminho);
+    List<String> listarDocumentos(String bucketNome, String subBucket);
+    void atualizarDocumento(String bucketNome,TipoDocumento tipoDocumento, String documentoNome, MultipartFile file);
 }
