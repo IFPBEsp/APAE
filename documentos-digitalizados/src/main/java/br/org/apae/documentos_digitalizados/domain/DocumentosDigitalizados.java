f29fe9f35f65adeec86cb7d52adb89ada4bd6fc8
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-02 14:03:23 -0300

    chore[domain]: mudança nos atributos do domain

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/DocumentosDigitalizados.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/DocumentosDigitalizados.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Entity
@@ -12,13 +14,26 @@ public class DocumentosDigitalizados {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String nomeBucket;
+
     @Column(nullable = false)
     private Long pacienteId;
+
+    @Column(nullable = false)
+    private String nomePaciente;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoPaciente tipoPaciente;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private TipoDocumento tipoDocumento;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String documento;
+
+    @Column(nullable = false)
+    private LocalDateTime dataAtualizacao;
 }
--- documentos-digitalizados/src/main/resources/db/migration/V1__create_documentos_table.sql
+++ documentos-digitalizados/src/main/resources/db/migration/V1__create_documentos_table.sql
@@ -1,6 +1,10 @@
 CREATE TABLE documentos_digitalizados (
-                                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                                          paciente_id BIGINT NOT NULL,
-                                          tipo_de_documento VARCHAR(255) NOT NULL,
-                                          documento VARCHAR(255) NOT NULL UNIQUE
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome_bucket VARCHAR(255) NOT NULL,
+    paciente_id BIGINT NOT NULL,
+    nome_paciente VARCHAR(255) NOT NULL,
+    tipo_paciente VARCHAR(255) NOT NULL,
+    tipo_documento VARCHAR(255) NOT NULL,
+    documento VARCHAR(255) NOT NULL,
+    data_atualizacao TIMESTAMP NOT NULL
 );
