cea6937561ca56ac14f3343a4c22c27b909bd7b4
isnael07 <isnaellima24@gmail.com>
2025-05-05 19:54:10 -0300

    chore[repository]: altera tipo do ID de paciente em findByPacientes

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/infrastructure/repository/DocumentosDigitalizadosRepository.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/infrastructure/repository/DocumentosDigitalizadosRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface DocumentosDigitalizadosRepository extends JpaRepository<DocumentosDigitalizados, Long> {
-    Optional<DocumentosDigitalizados> findByPacienteId(Long pacienteId);
+    Optional<DocumentosDigitalizados> findByPacienteId(UUID pacienteId);
 }
