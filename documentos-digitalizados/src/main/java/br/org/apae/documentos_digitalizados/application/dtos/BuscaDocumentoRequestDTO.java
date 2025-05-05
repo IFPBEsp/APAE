25baeca71975ce5902f226749afd23c2f7bd6ec2
isnael07 <isnaellima24@gmail.com>
2025-05-05 19:55:11 -0300

    chore[dto]: altera o anotação de NotBlank para NotNull

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/dtos/BuscaDocumentoRequestDTO.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/dtos/BuscaDocumentoRequestDTO.java
@@ -4,7 +4,9 @@ import br.org.apae.documentos_digitalizados.domain.TipoDocumento;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record BuscaDocumentoRequestDTO(@NotBlank(message = "ID do paciente é obrigatório!") Long idPaciente,
+import java.util.UUID;
+
+public record BuscaDocumentoRequestDTO(@NotNull(message = "ID do paciente é obrigatório!") UUID idPaciente,
                                        @NotBlank(message = "Nome do paciente é obrigatório!") String nomePaciente,
                                        @NotNull(message = "Tipo do documento é obrigatório!") TipoDocumento tipoDocumento,
                                        @NotBlank(message = "Nome do documento é obrigatório!") String nomeDocumento) {
