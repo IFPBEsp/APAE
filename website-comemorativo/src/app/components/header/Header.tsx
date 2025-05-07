192a22e6abcd9706c7227aa9087a4e22677ea125
RaykkonerD <raykonerd@gmail.com>
2025-05-07 14:17:53 -0300

    feat: adição de menu ao header

--- website-comemorativo/src/app/components/header/Header.tsx
+++ website-comemorativo/src/app/components/header/Header.tsx
@@ -1,38 +1,55 @@
-'use client';
+"use client";
 
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Styles from "./page.module.css";
-import { AlignJustify } from 'lucide-react';
-import { useState } from 'react';
+import { AlignJustify } from "lucide-react";
+import { useState } from "react";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 export default function Header() {
-    const pathname = usePathname();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const pathname = usePathname();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
 
-    return (
-        <>
-            <div className={Styles.header}>
-                <Image src="/APAE-logo.png" alt="Logo APAE" width={50} height={50} />
-                <div className={Styles.linksTelas}>
-                    <span className={pathname == "/" ? Styles.pageLink : Styles.link}>Página Inicial</span>
-                    <span className={pathname == "/30anos" ? Styles.pageLink : Styles.link}>30 Anos</span>
-                    <span className={pathname == "/contato" ? Styles.pageLink : Styles.link}>Contato</span>
-                </div>
-                <button className={Styles.hamburguer} onClick={toggleMenu}>
-                    <AlignJustify />
-                </button>
-            </div>
-            <div className={Styles.darkScreen}></div>
-            <div className={isMenuOpen ? Styles.menuOpen : Styles.menu}>
-                <p>Página Inicial</p>
-                <p>30 Anos</p>
-                <p>Contato</p>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className={Styles.header}>
+				<Image src="/logo.png" alt="Logo APAE" width={120} height={50} />
+				<div className={Styles.linksTelas}>
+					<span className={pathname == "/" ? Styles.linkAtivo : Styles.link}>
+						Página Inicial
+					</span>
+					<span
+						className={pathname == "/30anos" ? Styles.pageLink : Styles.link}
+					>
+						30 Anos
+					</span>
+					<span
+						className={pathname == "/contato" ? Styles.pageLink : Styles.link}
+					>
+						Contato
+					</span>
+				</div>
+				<button className={Styles.hamburguer} onClick={toggleMenu}>
+					<AlignJustify />
+				</button>
+			</div>
+			<SwipeableDrawer
+				anchor="right"
+				open={isMenuOpen}
+				onClose={toggleMenu}
+				onOpen={() => 0}
+			>
+				<ul className={Styles.menu}>
+					<li>Página Inicial</li>
+					<li>30 Anos</li>
+					<li>Contato</li>
+				</ul>
+			</SwipeableDrawer>
+		</>
+	);
 }
