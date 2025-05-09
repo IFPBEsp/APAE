cbf165a29deac6a64ee0b9084f9bf09c1da2c23c
dinizhugo <hugodiniztech01@gmail.com>
2025-05-09 14:25:47 -0300

    refactor[storage]: Mudando o pacote e alterando a interface do MinIO

---
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/domain/ports/storage/MinioStorage.java
@@ -0,0 +1,9 @@
+package br.org.apae.documentos_medicos.domain.ports.storage;
+
+import java.util.List;
+
+public interface MinioStorage {
+    void uploadFile(String bucket, String path, byte[] file);
+    List<String> listObject(String bucket, String prefix);
+    byte[] getMedicalDocumentByFileName(String bucket, String objectName);
+}
