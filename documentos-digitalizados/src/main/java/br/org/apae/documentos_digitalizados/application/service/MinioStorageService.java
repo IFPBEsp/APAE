e9eb5ff8d8ba25c00a87960cbeeb2324546543f8
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-08 21:35:23 -0300

    chore[service]: criação de bucket e verificação se existe bucket

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageService.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageService.java
@@ -1,9 +1,11 @@
 package br.org.apae.documentos_digitalizados.application.service;
 
+import java.util.UUID;
+
 public interface MinioStorageService {
-    void criarBucket(String bucketNome);
+    void criarBucket(UUID bucketNome);
     void listarBuckets();
     void listarBucketPorNome(String bucketNome);
-    void atualizarBucket(String bucketNome);
     void deletarBucket(String bucketNome);
+    boolean existeBucket(String bucketNome);
 }
--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageServiceImp.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/application/service/MinioStorageServiceImp.java
@@ -1,10 +1,15 @@
 package br.org.apae.documentos_digitalizados.application.service;
 
+import br.org.apae.documentos_digitalizados.domain.exception.DiretorioException;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -13,16 +18,15 @@ public class MinioStorageServiceImp implements MinioStorageService {
     private final MinioClient minioClient;
 
     @Override
-    public void criarBucket(String bucketNome)  {
-        boolean exists;
-        try {
-            exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketNome).build());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        if (!exists) {
+    public void criarBucket(UUID bucketNome) {
+        boolean existe = existeBucket(bucketNome.toString());
+
+        if (!existe) {
             try {
-                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketNome).build());
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketNome.toString()).build());
+                criarPasta(bucketNome.toString(), "documentos-pessoal/");
+                criarPasta(bucketNome.toString(), "documentos-medico/");
+                criarPasta(bucketNome.toString(), "documentos-escolar/");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -40,12 +44,30 @@ public class MinioStorageServiceImp implements MinioStorageService {
     }
 
     @Override
-    public void atualizarBucket(String bucketNome) {
+    public void deletarBucket(String bucketNome) {
 
     }
 
     @Override
-    public void deletarBucket(String bucketNome) {
+    public boolean existeBucket(String bucketNome) {
+        try {
+            return minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketNome).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private void criarPasta(String bucketNome, String nomePasta){
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketNome)
+                            .object(nomePasta)
+                            .stream(new ByteArrayInputStream(new byte[0]), 0, -1)
+                            .build()
+            );
+        } catch (Exception e){
+            throw new DiretorioException("Falha ao criar diretório!\n" + e);
+        }
     }
 }
