5e2dc168e97274f4b927439587505d6d3d9cad9d
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-09 15:24:27 -0300

    feat[domain]: exceções personalizadas criadas

---
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/CriacaoBucketException.java
@@ -0,0 +1,7 @@
+package br.org.apae.documentos_digitalizados.domain.exception;
+
+public class CriacaoBucketException extends RuntimeException {
+    public CriacaoBucketException(String message) {
+        super(message);
+    }
+}
