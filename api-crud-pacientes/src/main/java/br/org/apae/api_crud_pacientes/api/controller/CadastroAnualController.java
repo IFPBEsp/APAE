2bf364b23f2535ab77158105d3804c8a944feaf2
klebsonamarantes <klebson.silva@aluno.uepb.edu.br>
2025-05-08 23:39:54 -0300

    feat(cadastro-anual): criacao CadastroAnualController com endpoints REST

---
+++ api-crud-pacientes/src/main/java/br/org/apae/api_crud_pacientes/api/controller/CadastroAnualController.java
@@ -0,0 +1,47 @@
+package br.org.apae.api_crud_pacientes.api.controller;
+
+import br.org.apae.api_crud_pacientes.api.dtos.cadastro_anual.CadastroAnualRequest;
+import br.org.apae.api_crud_pacientes.api.dtos.cadastro_anual.CadastroAnualResponse;
+import br.org.apae.api_crud_pacientes.domain.service.CadastroAnualService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cadastros-anual")
+public class CadastroAnualController {
+
+    private final CadastroAnualService service;
+
+    public CadastroAnualController(CadastroAnualService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<CadastroAnualResponse> create(@Valid @RequestBody CadastroAnualRequest dto) {
+        return new ResponseEntity<>(service.create(dto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CadastroAnualResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CadastroAnualResponse>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CadastroAnualResponse> update(@PathVariable Long id, @Valid @RequestBody CadastroAnualRequest dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
