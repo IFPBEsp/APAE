cb6936fa29dfdedcd29debb04541a6069e2e8c49
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-09 16:11:22 -0300

    feat[domain]: criação de novas exceções personalizadas

---
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/DeletarBucketException.java
@@ -0,0 +1,7 @@
+package br.org.apae.documentos_digitalizados.domain.exception;
+
+public class DeletarBucketException extends RuntimeException {
+    public DeletarBucketException(String message) {
+        super(message);
+    }
+}
