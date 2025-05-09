1db0e060f6e1e5c941c4633ca4ba01ca3794da4b
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-08 21:42:52 -0300

    chore[controller]: criação de bucket e verificação se existe bucket

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosController.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosController.java
@@ -1,29 +1,26 @@
 package br.org.apae.documentos_digitalizados.api.controller;
 
-import br.org.apae.documentos_digitalizados.api.dto.AtualizarBucketRequestDTO;
-import br.org.apae.documentos_digitalizados.api.dto.CriarBucketRequestDTO;
 import br.org.apae.documentos_digitalizados.api.dto.ListagemBucketResponseDTO;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
-@RequestMapping("/documento")
 public interface DocumentosDigitalizadosController {
 
-    @PostMapping
-    ResponseEntity<Void> criarBucket(@RequestBody @Valid CriarBucketRequestDTO dto);
+    @PostMapping("/{bucketNome}")
+    ResponseEntity<Void> criarBucket(@PathVariable UUID bucketNome);
 
     @GetMapping
     ResponseEntity<List<ListagemBucketResponseDTO>> listarBuckets();
 
-    @GetMapping("/{bucket}")
-    ResponseEntity<ListagemBucketResponseDTO> listarBucket(@PathVariable String bucket);
+    @GetMapping("/{bucketNome}")
+    ResponseEntity<ListagemBucketResponseDTO> listarBucket(@PathVariable UUID bucketNome);
 
-    @PutMapping
-    ResponseEntity<Void> atualizarBucket(@RequestBody @Valid AtualizarBucketRequestDTO dto);
+    @DeleteMapping("/{bucketNome}")
+    ResponseEntity<Void> deletarBucket(@PathVariable UUID bucketNome);
 
-    @DeleteMapping("/{bucket}")
-    ResponseEntity<Void> deletarBucket(@PathVariable String bucket);
+    @GetMapping("/verificar/{bucketNome}")
+    ResponseEntity<Boolean> verificarBucket(@PathVariable UUID bucketNome);
 }
--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosControllerImp.java
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/api/controller/DocumentosDigitalizadosControllerImp.java
@@ -1,22 +1,29 @@
 package br.org.apae.documentos_digitalizados.api.controller;
 
-import br.org.apae.documentos_digitalizados.api.dto.AtualizarBucketRequestDTO;
-import br.org.apae.documentos_digitalizados.api.dto.CriarBucketRequestDTO;
 import br.org.apae.documentos_digitalizados.api.dto.ListagemBucketResponseDTO;
+import br.org.apae.documentos_digitalizados.application.service.MinioStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
+
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("/bucket")
 public class DocumentosDigitalizadosControllerImp implements DocumentosDigitalizadosController {
+
+    private final MinioStorageService minioStorageService;
+
     @Override
-    public ResponseEntity<Void> criarBucket(CriarBucketRequestDTO dto) {
-        return null;
+    public ResponseEntity<Void> criarBucket(UUID bucketNome) {
+        minioStorageService.criarBucket(bucketNome);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Override
@@ -25,17 +32,19 @@ public class DocumentosDigitalizadosControllerImp implements DocumentosDigitaliz
     }
 
     @Override
-    public ResponseEntity<ListagemBucketResponseDTO> listarBucket(String bucket) {
+    public ResponseEntity<ListagemBucketResponseDTO> listarBucket(UUID bucketNome) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Void> atualizarBucket(AtualizarBucketRequestDTO dto) {
+    public ResponseEntity<Void> deletarBucket(UUID bucketNome) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Void> deletarBucket(String bucket) {
-        return null;
+    public ResponseEntity<Boolean> verificarBucket(UUID bucketNome) {
+        boolean existeBucket = minioStorageService.existeBucket(bucketNome.toString());
+
+        return ResponseEntity.status(HttpStatus.OK).body(existeBucket);
     }
 }
