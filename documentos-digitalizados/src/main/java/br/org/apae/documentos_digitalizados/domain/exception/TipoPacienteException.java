fa06c63904c6726ab12ed1a38826cf19055a721c
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-08 21:34:35 -0300

    feat[domain]: criação de exceções personalizadas

---
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/TipoPacienteException.java
@@ -0,0 +1,7 @@
+package br.org.apae.documentos_digitalizados.domain.exception;
+
+public class TipoPacienteException extends RuntimeException {
+    public TipoPacienteException(String message) {
+        super(message);
+    }
+}
