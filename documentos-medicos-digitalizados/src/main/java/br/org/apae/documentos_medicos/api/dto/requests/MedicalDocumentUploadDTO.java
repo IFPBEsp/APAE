cbe63ae24173ab11aabd25650ae87e7abe219171
dinizhugo <hugodiniztech01@gmail.com>
2025-05-09 14:24:26 -0300

    refactor[dto]: Mudando o tipo UUID para String (#4)

--- documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/dto/requests/MedicalDocumentUploadDTO.java
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/dto/requests/MedicalDocumentUploadDTO.java
@@ -1,11 +1,9 @@
 package br.org.apae.documentos_medicos.api.dto.requests;
 
-import java.util.UUID;
-
 import org.springframework.web.multipart.MultipartFile;
 
 public record MedicalDocumentUploadDTO(
-    UUID patientId,
+    String patientId,
     Integer year,
     String documentType,
     MultipartFile file
