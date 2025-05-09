21a81214e7f9b6d219ec31c18758dee866458084
ismaellima4 <ismael.github@gmail.com>
2025-05-09 16:56:01 -0300

    feat[mapper]: inicializado a implementação do mapper

---
+++ documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/infrastructure/mapper/PersonalDocumentMapper.java
@@ -0,0 +1,19 @@
+package br.org.apae.documentos_pessoais_digitalizados.infrastructure.mapper;
+
+import br.org.apae.documentos_pessoais_digitalizados.api.dto.req.PersonalDocumentReqDTO;
+import br.org.apae.documentos_pessoais_digitalizados.api.dto.res.PersonalDocumentResDTO;
+import br.org.apae.documentos_pessoais_digitalizados.domain.model.PersonalDocument;
+
+import java.util.List;
+
+public class PersonalDocumentMapper {
+
+    public List<PersonalDocument> toEntity(PersonalDocumentReqDTO personalDocumentReqDTO) {
+        return null;
+    }
+
+    public PersonalDocumentResDTO toDTO(PersonalDocument personalDocument) {
+        return null;
+    }
+
+}
