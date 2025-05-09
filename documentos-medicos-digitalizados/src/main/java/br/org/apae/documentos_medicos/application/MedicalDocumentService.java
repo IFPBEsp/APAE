4e9de61dade308ad78808bfb762b6960ccedd996
dinizhugo <hugodiniztech01@gmail.com>
2025-05-09 14:26:48 -0300

    feat[service]: Criando a interface para o application service (#4)

--- documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/application/MedicalDocumentService.java
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/application/MedicalDocumentService.java
@@ -1,5 +1,17 @@
 package br.org.apae.documentos_medicos.application;
 
-public class MedicalDocumentService {
-    
+import java.util.List;
+
+import br.org.apae.documentos_medicos.api.dto.requests.MedicalDocumentUploadDTO;
+import br.org.apae.documentos_medicos.api.dto.responses.MedicalDocumentResponseDTO;
+import br.org.apae.documentos_medicos.domain.models.MedcialDocumentType;
+
+public interface MedicalDocumentService {
+
+    void saveFile(MedicalDocumentUploadDTO dtoObject);
+
+    List<MedicalDocumentResponseDTO> listMedicalDocument(String patientId, Integer year);
+
+    List<MedicalDocumentResponseDTO> listMedicalDocumentByType(String patientId, Integer year, MedcialDocumentType type);
+
 }
