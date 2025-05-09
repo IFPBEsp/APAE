34a0f8ecd723fd55068bdf295542bcb112d1e993
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-08 21:43:20 -0300

    remove[domain]: remoção de exceção

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/TipoPacienteException.java
+++
@@ -1,7 +0,0 @@
-package br.org.apae.documentos_digitalizados.domain.exception;
-
-public class TipoPacienteException extends RuntimeException {
-    public TipoPacienteException(String message) {
-        super(message);
-    }
-}
