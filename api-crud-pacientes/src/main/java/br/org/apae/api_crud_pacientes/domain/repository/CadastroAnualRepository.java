6aa67e919c7283bc246fbd8cb3d0c37baafc0019
klebsonamarantes <klebson.silva@aluno.uepb.edu.br>
2025-05-08 23:39:10 -0300

    feat(cadastro-anual): criacao CadastroAnualRepository interface

---
+++ api-crud-pacientes/src/main/java/br/org/apae/api_crud_pacientes/domain/repository/CadastroAnualRepository.java
@@ -0,0 +1,9 @@
+package br.org.apae.api_crud_pacientes.domain.repository;
+
+import br.org.apae.api_crud_pacientes.domain.model.CadastroAnual;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CadastroAnualRepository extends JpaRepository<CadastroAnual, Long> {
+}
