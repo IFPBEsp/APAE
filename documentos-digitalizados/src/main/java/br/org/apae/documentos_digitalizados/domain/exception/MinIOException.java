40696b594f1469b17161bd899b5037fc892a4628
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-10 15:44:20 -0300

    feat[domain]: criadas exceções novas

---
+++ documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/MinIOException.java
@@ -0,0 +1,7 @@
+package br.org.apae.documentos_digitalizados.domain.exception;
+
+public class MinIOException extends RuntimeException {
+    public MinIOException(String message) {
+        super(message);
+    }
+}
