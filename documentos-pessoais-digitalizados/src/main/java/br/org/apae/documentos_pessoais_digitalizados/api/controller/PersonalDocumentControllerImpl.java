64cb182663c3a9752164a59c9da937f65db6c02b
viniciusdelucena <viniciusmelosegundo@gmail.com>
2025-05-09 22:28:57 -0300

    feat[controller]: iniciado implementação do controller

--- documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/api/controller/PersonalDocumentControllerImpl.java
+++ documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/api/controller/PersonalDocumentControllerImpl.java
@@ -4,58 +4,69 @@ import br.org.apae.documentos_pessoais_digitalizados.api.dto.req.PersonalDocumen
 import br.org.apae.documentos_pessoais_digitalizados.api.dto.res.PersonalDocumentResDTO;
 import br.org.apae.documentos_pessoais_digitalizados.application.service.PersonalDocumentService;
 import br.org.apae.documentos_pessoais_digitalizados.application.service.StorageService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/personal-documents")
 public class PersonalDocumentControllerImpl implements PersonalDocumentController {
 
     private final PersonalDocumentService personalDocumentService;
     private final StorageService storageService;
 
     public PersonalDocumentControllerImpl(PersonalDocumentService personalDocumentService,
-                                          StorageService storageService) {
+        StorageService storageService) {
         this.personalDocumentService = personalDocumentService;
         this.storageService = storageService;
     }
 
+    @PostMapping
     @Override
-    public ResponseEntity<List<PersonalDocumentResDTO>> create(PersonalDocumentReqDTO personalDocumentReqDTO) {
-        // Chama o serviço para criar os documentos pessoais
-       return null;
+      public ResponseEntity<List<PersonalDocumentResDTO>> create(@RequestBody PersonalDocumentReqDTO personalDocumentReqDTO) {
+        List<PersonalDocumentResDTO> created = personalDocumentService.create(personalDocumentReqDTO);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
+    @PutMapping("/{id}")
     @Override
-    public ResponseEntity<PersonalDocumentResDTO> update(UUID id, PersonalDocumentReqDTO personalDocumentReqDTO) {
-        // TODO: Implementar atualização de documento pessoal
-        return null;
+    public ResponseEntity<PersonalDocumentResDTO> update(@PathVariable UUID id,
+        @RequestBody PersonalDocumentReqDTO reqDTO) {
+        PersonalDocumentResDTO updated = personalDocumentService.update(id, reqDTO);
+        return ResponseEntity.ok(updated);
     }
 
+    @GetMapping("/{id}")
     @Override
-    public ResponseEntity<PersonalDocumentResDTO> findById(UUID id) {
-        // TODO: Implementar busca por ID de documento pessoal
-        return null;
+    public ResponseEntity<PersonalDocumentResDTO> findById(@PathVariable UUID id) {
+        PersonalDocumentResDTO dto = personalDocumentService.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
+    @GetMapping
     @Override
     public ResponseEntity<List<PersonalDocumentResDTO>> findAll() {
-        // TODO: Implementar listagem de todos os documentos pessoais
+        List<PersonalDocumentResDTO> all = personalDocumentService.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @DeleteMapping("/{id}")
+    @Override
+    public ResponseEntity<PersonalDocumentResDTO> delete(@PathVariable UUID id) {
+        PersonalDocumentResDTO deleted = personalDocumentService.findById(id);
+
+        //to-do
         return null;
     }
 
+   @GetMapping("/{id}/file")
     @Override
-    public ResponseEntity<PersonalDocumentResDTO> delete(UUID id) {
-        // TODO: Implementar exclusão de documento pessoal
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<byte[]> findDocumentById(UUID id) {
-        // TODO: Implementar busca de arquivo de documento pelo ID
-        return null;
+    public ResponseEntity<byte[]> findDocumentById(@PathVariable UUID id) {
+    ///to-do
+    return null;
     }
 }
