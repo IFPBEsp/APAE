d478129d1de5f43ea69068cada179ca17c7a6047
ismaellima4 <ismael.github@gmail.com>
2025-05-10 14:44:15 -0300

    feat[mapper]: adicionando o mapper para PersonalDocument

--- documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/infrastructure/mapper/PersonalDocumentMapper.java
+++ documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/infrastructure/mapper/PersonalDocumentMapper.java
@@ -1,19 +1,14 @@
 package br.org.apae.documentos_pessoais_digitalizados.infrastructure.mapper;
 
-import br.org.apae.documentos_pessoais_digitalizados.api.dto.req.PersonalDocumentReqDTO;
-import br.org.apae.documentos_pessoais_digitalizados.api.dto.res.PersonalDocumentResDTO;
+import br.org.apae.documentos_pessoais_digitalizados.api.dto.res.PersonalDocumentResUrlDTO;
 import br.org.apae.documentos_pessoais_digitalizados.domain.model.PersonalDocument;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Component
 public class PersonalDocumentMapper {
-
-    public List<PersonalDocument> toEntity(PersonalDocumentReqDTO personalDocumentReqDTO) {
-        return null;
-    }
-
-    public PersonalDocumentResDTO toDTO(PersonalDocument personalDocument) {
-        return null;
+    public PersonalDocumentResUrlDTO toDTO(PersonalDocument personalDocument) {
+        String url = "/api/documents/" + personalDocument.getPatient() + "/file";
+        return new PersonalDocumentResUrlDTO(url);
     }
 
 }
