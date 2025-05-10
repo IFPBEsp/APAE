36ac1569ce6395e98b87381554a39bf3b3e749f0
dinizhugo <hugodiniztech01@gmail.com>
2025-05-10 00:00:54 -0300

    feat[service]: Implementando o método de salvar para testar a comunicação com o MinIO (#4)

--- documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/application/MedicalDocumentServiceImpl.java
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/application/MedicalDocumentServiceImpl.java
@@ -1,30 +1,66 @@
 package br.org.apae.documentos_medicos.application;
 
-import java.util.List;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.org.apae.documentos_medicos.api.dto.requests.MedicalDocumentUploadDTO;
 import br.org.apae.documentos_medicos.api.dto.responses.MedicalDocumentResponseDTO;
 import br.org.apae.documentos_medicos.domain.models.MedcialDocumentType;
+import br.org.apae.documentos_medicos.domain.ports.storage.MinioStorage;
+import br.org.apae.documentos_medicos.infrastructure.client.StorageClient;
 
+@Service
 public class MedicalDocumentServiceImpl implements MedicalDocumentService {
 
-    @Override
-    public void saveFile(MedicalDocumentUploadDTO dtoObject) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'saveFile'");
+    private MinioStorage minioMedicalDocumentStorage;
+    private StorageClient storageClient;
+    private static final String FOLDER_NAME = "documentos-medicos";
+
+    @Autowired
+    public MedicalDocumentServiceImpl(MinioStorage minioMedicalDocumentStorage, StorageClient storageClient) {
+        this.minioMedicalDocumentStorage = minioMedicalDocumentStorage;
+        this.storageClient = storageClient;
     }
 
     @Override
-    public List<MedicalDocumentResponseDTO> listMedicalDocument(String patientId, Integer year) {
+    public void saveFile(MedicalDocumentUploadDTO dtoObject, MultipartFile multipartFile) {
+        // validateBucket(dtoObject.patientId());
+
+        try {
+            String bucket = dtoObject.getPatientId();
+            String path = FOLDER_NAME + "/" + 
+                            dtoObject.getYear() + "/" +
+                            MedcialDocumentType.valueOf(dtoObject.getDocumentType()).getPrefix() + "/" + 
+                            multipartFile.getOriginalFilename();
+                            
+            byte[] file = multipartFile.getBytes();
+
+            minioMedicalDocumentStorage.uploadFile(bucket, path, file);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao processar o arquivo: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public MedicalDocumentResponseDTO listMedicalDocument(String patientId, Integer year) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listMedicalDocument'");
     }
 
     @Override
-    public List<MedicalDocumentResponseDTO> listMedicalDocumentByType(String patientId, Integer year,
+    public MedicalDocumentResponseDTO listMedicalDocumentByType(String patientId, Integer year,
             MedcialDocumentType type) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listMedicalDocumentByType'");
     }
 
+    private void validateBucket(String bucket) {
+        if (!storageClient.bucketExists(bucket)) {
+            throw new RuntimeException("Bucket não existe: " + bucket);
+        }
+    }
 }
