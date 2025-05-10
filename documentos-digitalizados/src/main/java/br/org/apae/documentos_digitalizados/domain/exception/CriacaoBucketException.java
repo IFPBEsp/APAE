bdbb4453707ff3bca56ca04559d0def5801b1985
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-10 15:44:41 -0300

    remove[domain]: removido exceção

--- documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/CriacaoBucketException.java
+++
@@ -1,7 +0,0 @@
-package br.org.apae.documentos_digitalizados.domain.exception;
-
-public class CriacaoBucketException extends RuntimeException {
-    public CriacaoBucketException(String message) {
-        super(message);
-    }
-}
