5fc2c94b9cf755afab13d509be4fad4f25e49bd1
ismaellima4 <ismael.github@gmail.com>
2025-05-11 16:07:22 -0300

    chore[dependency]: adicionado dependencia de valiadacao e documentacao da api

--- documentos-pessoais-digitalizados/build.gradle.kts
+++ documentos-pessoais-digitalizados/build.gradle.kts
@@ -21,9 +21,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.minio:minio:8.5.17")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	runtimeOnly("com.h2database:h2")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 }
 
 tasks.withType<Test> {
