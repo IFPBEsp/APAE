f4d8069025f15521b7fd75852fbbdddc9ec53dff
ViniciusCavalcantePequeno <vinicius.pequeno@academico.ifpb.edu.br>
2025-05-11 16:21:03 -0300

    feat: Implementação da pagina de contato e correção do footer  Co-authored-by: Pedroo722 <pedro.rocha@academico.ifpb.edu.br>

--- website-comemorativo/src/app/components/Footer.tsx
+++ website-comemorativo/src/app/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { colors } from '@mui/material';
 import React from 'react';
 
 interface FooterProps {}
@@ -7,18 +8,18 @@ const Footer: React.FC<FooterProps> = () => {
     <footer style={styles.footer}>
       <div style={styles.section}>
         <p><b>Telefone:</b></p>
-        <a href='https://wa.me/83993833950' style={{ marginTop: '5px' }}><b>(83) 9 9383-3950</b></a>
+        <a href='https://wa.me/83993833950' style={{ marginTop: '5px', color: '#fff' }}><b>(83) 9 9383-3950</b></a>
       </div>
       <div style={styles.section}>
         <p><b>Siga-nos</b></p>
-        <a href='https://instagram.com/apaeesperanca_' style={{ marginTop: '5px' }}><b>@apaeesperanca_</b></a>
+        <a href='https://instagram.com/apaeesperanca_' style={{ marginTop: '5px', color: '#fff' }}><b>@apaeesperanca_</b></a>
       </div>
       <div style={styles.section}>
         <p><b>Nossa Localização</b></p>
-        <a href='https://maps.app.goo.gl/5JwRebJbASuFdqo6A' style={{ marginTop: '5px' }}>
+        <a href='https://maps.app.goo.gl/5JwRebJbASuFdqo6A' style={{ marginTop: '5px', color: '#fff' }}>
           <b>
             Rua Santo Antonio, 491
-            <br></br>
+            <br />
             Centro, Esperança, Paraíba, Brasil
           </b>
         </a>
@@ -27,21 +28,39 @@ const Footer: React.FC<FooterProps> = () => {
   );
 };
 
-
 const styles = {
   footer: {
-    padding: '15px 500px',
-    width: '100vw',
+    padding: '15px 30px', 
+    width: '100%',
     backgroundColor: '#165BAA',
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'center', 
+    alignItems: 'center', 
     flexWrap: 'wrap' as 'wrap',
     borderTop: '1px solid #ddd',
+    color: '#fff',
   },
   section: {
     flex: '1 1 30%',
     padding: '10px',
     marginBottom: '20px',
+    textAlign: 'center', 
+    color: '#fff',
+  },
+};
+
+// Responsividade: para telas menores
+const mediaQueries = {
+  '@media (max-width: 768px)': {
+    footer: {
+      padding: '15px 20px', 
+      display: 'block', 
+      textAlign: 'center', 
+    },
+    section: {
+      flex: '1 1 100%', 
+      textAlign: 'center', 
+    },
   },
 };
 
