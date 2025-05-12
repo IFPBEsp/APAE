1cbd6b8bf36e24d0d15b4e0d6b81b86636e713fc
viniciusdelucena <viniciusmelosegundo@gmail.com>
2025-05-11 23:12:20 -0300

    feat[documentation]: implementa SwaggerConfig

--- documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/infrastructure/documentation/SwaggerConfig.java
+++ documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/infrastructure/documentation/SwaggerConfig.java
@@ -1,3 +1,19 @@
+package br.org.apae.documentos_pessoais_digitalizados.infrastructure.documentation;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
-    
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("API documentos pessoais digitalizados")
+                .version("1.0")
+                .description("API tem a responsabilidade de salvar documentos pessoais de um determinado paciente "));
+    }
 }
