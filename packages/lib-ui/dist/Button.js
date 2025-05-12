a831eacbcf01aa6cfdf2e2860a9c1855118c57e5
FihFafnir <79815557+FihFafnir@users.noreply.github.com>
2025-05-12 14:19:41 +0000

    chore: fix linting issues

--- packages/lib-ui/dist/Button.js
+++ packages/lib-ui/dist/Button.js
@@ -3,6 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Button = Button;
 var jsx_runtime_1 = require("react/jsx-runtime");
 function Button(props) {
-    return (0, jsx_runtime_1.jsx)("button", { onClick: function () { return props.onClick(); }, children: props.children });
+  return (0, jsx_runtime_1.jsx)("button", {
+    onClick: function () {
+      return props.onClick();
+    },
+    children: props.children,
+  });
 }
 exports.default = Button;
