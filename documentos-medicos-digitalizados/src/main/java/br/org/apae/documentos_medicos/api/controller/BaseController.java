61dee0fa666d146beaf935609624ed16a4f86dd5
dinizhugo <hugodiniztech01@gmail.com>
2025-05-12 22:54:48 -0300

    refactor[controller]: Corrigindo e alterando os nomes dos métodos da interface (#4)

--- documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/controller/BaseController.java
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/controller/BaseController.java
@@ -1,47 +1,43 @@
 package br.org.apae.documentos_medicos.api.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 
 import org.springframework.web.multipart.MultipartFile;
-
-
 import br.org.apae.documentos_medicos.api.dto.responses.MedicalDocumentResponseDTO;
-import br.org.apae.documentos_medicos.domain.models.MedcialDocumentType;
 
-@RequestMapping("/api/v1/documentos-medicos")
 public interface BaseController {
 
-    /*
-    * POST (/api/pacientes/{pacienteId}/documentos-medicos) - Anexar documentos médicos a um paciente.
-    * GET (/api/pacientes/{pacienteId}/documentos-medicos) - Listar todos os documentos médicos associados a 1 paciente.
-    * GET (/api/pacientes/{pacienteId}/documentos-medicos?tipo={tipo}) - Filtrar por tipo de documento.
-    * GET (/api/pacientes/{pacienteId}/documentos-medicos/historico?tipo={tipo}) - Visualizar histórico de um tipo de documento específico da parte médica.
-    * GET (/api/pacientes/{pacienteId}/documentos-medicos/{documentoId}) - Visualizar um documento médico específico do paciente.
-    * DELETE (/api/pacientes/{pacienteId}/documentos-medicos/{documentoId}) - Deletar documento.
-    */
+    ResponseEntity<Void> attachMedicalDocument(
+        @RequestPart String patientId, 
+        @RequestPart String year, 
+        @RequestPart String documentType, 
+        @RequestPart MultipartFile file
+    );
 
-    ResponseEntity<Void> uploadMedicalDocument(@RequestPart String patientId, @RequestPart String year, @RequestPart String documentType, @RequestPart MultipartFile file);
+    ResponseEntity<MedicalDocumentResponseDTO> listMedicalDocuments(
+        @PathVariable String patientId, 
+        @PathVariable String year
+    );
 
-    @GetMapping(params = "!tipo")
-    ResponseEntity<MedicalDocumentResponseDTO> listMedicalDocuments(@PathVariable String patientId, @PathVariable String year);
+    ResponseEntity<MedicalDocumentResponseDTO> listMedicalDocumentByType(
+        @PathVariable String patientId, 
+        @PathVariable String year,
+        @RequestParam(required = false) String tipoDocumento
+    );
 
-    @GetMapping(params = "tipo")
-    ResponseEntity<MedicalDocumentResponseDTO> listMedicalDocumentByType(@PathVariable String patientId, @PathVariable Integer year, @RequestParam(required = false) MedcialDocumentType tipoDocumento);
+    ResponseEntity<MedicalDocumentResponseDTO> listDocumentHistoryByType(
+        @PathVariable String patientId, 
+        @RequestParam String tipoDocumento
+    );
 
-    @GetMapping("/historico")
-    ResponseEntity<MedicalDocumentResponseDTO> historyDocumentsByType(@PathVariable String patientId, @RequestParam MedcialDocumentType tipoDocumento);
+    ResponseEntity<byte[]> viewPatientMedicalDocument(
+        @PathVariable String patientId, 
+        @RequestBody String fileName
+    );
 
-    @GetMapping("/{documentoId}")
-    ResponseEntity<MedicalDocumentResponseDTO> viewPatientDocument(@PathVariable String patientId, @PathVariable String documentoId);
-
-    @PatchMapping("/{documentoId}")
-    ResponseEntity<Void> deleteDocument(@PathVariable String patientId, @PathVariable String documentId);
+    ResponseEntity<Void> deleteDocument(@PathVariable String patientId, @RequestBody String fileName);
 }
