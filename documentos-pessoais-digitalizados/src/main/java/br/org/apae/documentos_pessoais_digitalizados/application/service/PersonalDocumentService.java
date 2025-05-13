e45a8cf7fc8673f58f61548b8cb5af9ce1eea2a7
ismaellima4 <ismael.github@gmail.com>
2025-05-13 12:09:50 -0300

    refactor[service]: removendo imports que nao estao sendo usados

--- documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/application/service/PersonalDocumentService.java
+++ documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/application/service/PersonalDocumentService.java
@@ -3,7 +3,6 @@ package br.org.apae.documentos_pessoais_digitalizados.application.service;
 import br.org.apae.documentos_pessoais_digitalizados.api.dto.req.PersonalDocumentFileReqDTO;
 import br.org.apae.documentos_pessoais_digitalizados.api.dto.req.PersonalDocumentReqDTO;
 import br.org.apae.documentos_pessoais_digitalizados.api.dto.res.PersonalDocumentResUrlDTO;
-import br.org.apae.documentos_pessoais_digitalizados.domain.model.PersonalDocument;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
