1c8063322d685d9a745bdf454dc4268f4c9faefd
dinizhugo <hugodiniztech01@gmail.com>
2025-05-13 20:35:32 -0300

    refactor[dto]: Removendo as anotações dos validations (#4)

--- documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/dto/responses/PatientPresignedUrlsDTO.java
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/dto/responses/PatientPresignedUrlsDTO.java
@@ -1,11 +1,6 @@
 package br.org.apae.documentos_medicos.api.dto.responses;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record PatientPresignedUrlsDTO(
-    @NotBlank(message = "O nome do arquivo é obrigatório.")
     String fileName,
-
-    @NotBlank(message = "O link é obrigatório.")
     String link
 ) {}
