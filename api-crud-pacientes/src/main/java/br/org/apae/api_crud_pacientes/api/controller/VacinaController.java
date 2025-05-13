2c66b86445fa7fce96c84500479083999f518eaa
meduarda18 <mariaeduardac604@gmail.com>
2025-05-12 22:29:45 -0300

    feat[controller]: criação do controller da entidade Vacina

---
+++ api-crud-pacientes/src/main/java/br/org/apae/api_crud_pacientes/api/controller/VacinaController.java
@@ -0,0 +1,54 @@
+package br.org.apae.api_crud_pacientes.api.controller;
+
+import br.org.apae.api_crud_pacientes.api.dtos.vacina.VacinaRequest;
+import br.org.apae.api_crud_pacientes.api.dtos.vacina.VacinaResponse;
+import br.org.apae.api_crud_pacientes.domain.service.VacinaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/vacinas")
+public class VacinaController {
+
+    private final VacinaService vacinaService;
+
+    public VacinaController(VacinaService vacinaService) {
+        this.vacinaService = vacinaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<VacinaResponse> criar(@RequestBody VacinaRequest request, UriComponentsBuilder uriBuilder) {
+        VacinaResponse response = vacinaService.create(request);
+        URI uri = uriBuilder.path("/vacinas/{id}").buildAndExpand(response.getId()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VacinaResponse>> listarTodas() {
+        List<VacinaResponse> vacinas = vacinaService.getAll();
+        return ResponseEntity.ok(vacinas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VacinaResponse> buscarPorId(@PathVariable UUID id) {
+        VacinaResponse response = vacinaService.getById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<VacinaResponse> atualizar(@PathVariable UUID id, @RequestBody VacinaRequest request) {
+        VacinaResponse response = vacinaService.update(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable UUID id) {
+        vacinaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
