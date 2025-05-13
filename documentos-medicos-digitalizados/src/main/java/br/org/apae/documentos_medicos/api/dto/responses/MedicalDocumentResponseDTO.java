30e43efc4104cdd1b19beda8c728371b6dcdffe7
dinizhugo <hugodiniztech01@gmail.com>
2025-05-13 20:37:07 -0300

    refactor[dto]: removendo as anotações do responseDTO (#4)

--- documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/dto/responses/MedicalDocumentResponseDTO.java
+++ documentos-medicos-digitalizados/src/main/java/br/org/apae/documentos_medicos/api/dto/responses/MedicalDocumentResponseDTO.java
@@ -2,12 +2,7 @@ package br.org.apae.documentos_medicos.api.dto.responses;
 
 import java.util.List;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record MedicalDocumentResponseDTO(
-    @NotBlank(message = "O ID é obrigatório.")
     String patientId,
-
-    @NotBlank(message = "A URL é obrigatória.")
     List<PatientPresignedUrlsDTO> urls
 ) {}
