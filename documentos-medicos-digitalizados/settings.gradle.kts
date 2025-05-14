815a0ac42fb277663899323ee5eeb255359a46d0
anielmelo <anielcontato@gmail.com>
2025-05-13 23:10:18 -0300

    chore: remove código desatualizado

--- documentos-medicos-digitalizados/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "documentos_medicos"
--- documentos-pessoais-digitalizados/src/main/java/br/org/apae/documentos_pessoais_digitalizados/application/exception/DocumentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.org.apae.documentos_pessoais_digitalizados.application.exception;
-
-public class DocumentNotFoundException extends RuntimeException {
-    public DocumentNotFoundException() {
-        super("Documento não encontrado ou não existe");
-    }
-}
