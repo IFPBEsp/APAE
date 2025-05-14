1f7b4b62c4901141517d6fcf076ffc9f955d384b
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-14 19:34:35 -0300

    feat[controller]: criação da interface de endpoints

--- api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresController.java
+++ api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresController.java
@@ -1,7 +1,41 @@
 package br.org.apae.documentos_escolares.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.org.apae.documentos_escolares.api.dto.request.DocumentoEscolarUploadRequestDTO;
+import br.org.apae.documentos_escolares.api.dto.response.DocumentoEscolarResponseDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
-public class DocumentosEscolaresController {
+import java.util.UUID;
+
+public interface DocumentosEscolaresController {
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<Void> anexarDocumentoEscolar(
+            @RequestPart("dto") DocumentoEscolarUploadRequestDTO dto,
+            @RequestPart("arquivo") MultipartFile arquivo
+    );
+
+    @GetMapping("/{pacienteId}/filtrar")
+    ResponseEntity<String> listarDocumentosEscolares(
+            @PathVariable("pacienteId") UUID pacienteId,
+            @RequestParam(value = "ano", required = false) Integer ano
+    );
+
+    @GetMapping("/{pacienteId}/historico")
+    ResponseEntity<DocumentoEscolarResponseDTO> historicoDocumentoEscolares(
+            @PathVariable("pacienteId") UUID pacienteId
+    );
+
+    @GetMapping("/{pacienteId}/visualizar")
+    ResponseEntity<byte[]> visualizarDocumentoEscolar(
+            @PathVariable("pacienteId") UUID pacienteId,
+            @RequestParam String nomeArquivo
+    );
+
+    @DeleteMapping("/{pacienteId}")
+    ResponseEntity<Void> deletarDocumentoEscolar(
+            @PathVariable("pacienteId") UUID pacienteId,
+            @RequestParam String nomeArquivo
+    );
 }
