d6978befff3fdbfc77445285ba7395cf7b8c1266
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-15 17:04:31 -0300

    feat[controller]: implementado o end point para anexar o documento escolar

---
+++ api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresControllerImp.java
@@ -0,0 +1,48 @@
+package br.org.apae.documentos_escolares.api.controller;
+
+import br.org.apae.documentos_escolares.api.dto.request.DocumentoEscolarUploadRequestDTO;
+import br.org.apae.documentos_escolares.api.dto.response.DocumentoEscolarResponseDTO;
+import br.org.apae.documentos_escolares.application.service.DocumentosEscolaresService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/documentos-escolares")
+public class DocumentosEscolaresControllerImp implements DocumentosEscolaresController {
+
+    private final DocumentosEscolaresService documentosEscolaresService;
+
+    @Override
+    public ResponseEntity<Void> anexarDocumentoEscolar(DocumentoEscolarUploadRequestDTO dto, MultipartFile arquivo) {
+        documentosEscolaresService.salvarArquivo(dto, arquivo);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Override
+    public ResponseEntity<String> listarDocumentosEscolares(UUID pacienteId, Integer ano) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<DocumentoEscolarResponseDTO> historicoDocumentoEscolares(UUID pacienteId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<byte[]> visualizarDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deletarDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
+        return null;
+    }
+}
