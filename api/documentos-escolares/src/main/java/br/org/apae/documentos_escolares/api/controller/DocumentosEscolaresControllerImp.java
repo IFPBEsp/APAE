17b60741a497402a18b3e3ba9351e2161690f65a
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-15 17:36:54 -0300

    feat[controller]: implementado o end point para visualizar ou fazer download do documento específico

--- api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresControllerImp.java
+++ api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresControllerImp.java
@@ -2,9 +2,12 @@ package br.org.apae.documentos_escolares.api.controller;
 
 import br.org.apae.documentos_escolares.api.dto.request.DocumentoEscolarUploadRequestDTO;
 import br.org.apae.documentos_escolares.api.dto.response.DocumentoEscolarResponseDTO;
+import br.org.apae.documentos_escolares.api.dto.response.VisualizacaoDocumentoEscolarResponseDTO;
 import br.org.apae.documentos_escolares.application.service.DocumentosEscolaresService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,11 +50,30 @@ public class DocumentosEscolaresControllerImp implements DocumentosEscolaresCont
 
     @Override
     public ResponseEntity<byte[]> visualizarDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
-        return null;
+        byte[] response = documentosEscolaresService.visualizarDocumentoEscolar(pacienteId, nomeArquivo);
+
+        MediaType contentType = getContentType(nomeArquivo);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(contentType)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + nomeArquivo + "\"")
+                .body(response);
     }
 
     @Override
     public ResponseEntity<Void> deletarDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
         return null;
+    }
+
+    private MediaType getContentType(String fileName) {
+        if (fileName.endsWith(".pdf")) {
+            return MediaType.APPLICATION_PDF;
+        } else if (fileName.endsWith(".png")) {
+            return MediaType.IMAGE_PNG;
+        } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+            return MediaType.IMAGE_JPEG;
+        } else {
+            return MediaType.APPLICATION_OCTET_STREAM;
+        }
     }
 }
