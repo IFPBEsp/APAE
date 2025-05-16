9a4d143dd2b6676d6241dabf369d4aae8abd5a59
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-16 17:28:58 -0300

    feat[domain]: adicionado algumas exceções personalizadas

--- api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/domain/exception/exceção.java
+++
@@ -1,7 +0,0 @@
-package br.org.apae.documentos_escolares.domain.exception;
-
-public class exceção extends RuntimeException {
-    public exceção(String message) {
-        super(message);
-    }
-}
