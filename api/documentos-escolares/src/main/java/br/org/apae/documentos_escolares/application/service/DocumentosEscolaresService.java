933047c0d9afe17cd5fc2d39c7166cfd48fd982d
klebsonamarantes <klebson.silva@aluno.uepb.edu.br>
2025-05-16 22:40:23 -0300

    feat[service]: criacao da interface de documentos escolares

--- api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/application/service/DocumentosEscolaresService.java
+++ api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/application/service/DocumentosEscolaresService.java
@@ -1,7 +1,22 @@
 package br.org.apae.documentos_escolares.application.service;
 
-import org.springframework.stereotype.Service;
+import br.org.apae.documentos_escolares.api.dto.request.DocumentoEscolarUploadRequestDTO;
+import br.org.apae.documentos_escolares.api.dto.response.DocumentoEscolarResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
 
-@Service
-public class DocumentosEscolaresService {
+import java.io.IOException;
+import java.util.UUID;
+
+public interface DocumentosEscolaresService {
+    void salvarArquivo(DocumentoEscolarUploadRequestDTO dto, MultipartFile arquivo);
+
+    DocumentoEscolarResponseDTO listarDocumentosEscolares(UUID pacienteId);
+
+    DocumentoEscolarResponseDTO listarDocumentosEscolaresAno(UUID pacienteId, Integer ano);
+
+    DocumentoEscolarResponseDTO historicoDocumentosEscolares(UUID pacienteId);
+
+    byte[] visualizarDocumentoEscolar(UUID pacienteId, String nomeArquivo);
+
+    void deletarDocumentoEscolar(UUID pacienteId, String nomeArquivo);
 }
