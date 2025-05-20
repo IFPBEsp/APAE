d3a73e470a863f1b3f58067e816e04cd82ef522c
lucasgomes14 <lucazmatehus14@gmail.com>
2025-05-20 14:41:48 -0300

    chore[controller]: implementado o método de atualização de documento

--- api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresControllerImp.java
+++ api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/controller/DocumentosEscolaresControllerImp.java
@@ -1,11 +1,9 @@
 package br.org.apae.documentos_escolares.api.controller;
 
+import br.org.apae.documentos_escolares.api.dto.request.DocumentoEscolarUpdateRequestDTO;
 import br.org.apae.documentos_escolares.api.dto.request.DocumentoEscolarUploadRequestDTO;
 import br.org.apae.documentos_escolares.api.dto.response.DocumentoEscolarResponseDTO;
-import br.org.apae.documentos_escolares.application.service.DocumentosEscolaresService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import br.org.apae.documentos_escolares.application.service.minio.MinioStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -23,59 +20,35 @@ import java.util.UUID;
 @RequestMapping("/api/documentos-escolares")
 public class DocumentosEscolaresControllerImp implements DocumentosEscolaresController {
 
-    private final DocumentosEscolaresService documentosEscolaresService;
+    private final MinioStorageService minioStorageService;
 
-    @Operation(summary = "Anexa o documento no bucket/documentos-escolar/ano", description = "Recebe por parâmetro o UUID do paciênte e o documento")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Documento anexado com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Erro ao anexar documento"),
-            @ApiResponse(responseCode = "500", description = "Documento não anexado por erro do minIO")
-    })
     @Override
     public ResponseEntity<Void> anexarDocumentoEscolar(DocumentoEscolarUploadRequestDTO dto, MultipartFile arquivo)  {
-        documentosEscolaresService.salvarArquivo(dto, arquivo);
+        minioStorageService.salvarArquivo(dto, arquivo);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @Operation(summary = "Retorna listagem de todos ou por ano documentos de um paciênte", description = "Recebe por parâmetro o UUID do paciênte e um parâmetro opcional \"ano\"")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Documentos listados com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Paciênte não encontrado"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do minIO")
-    })
     @Override
     public ResponseEntity<DocumentoEscolarResponseDTO> listarDocumentosEscolares(UUID pacienteId, Integer ano) {
-        DocumentoEscolarResponseDTO responseDTO = documentosEscolaresService.listarDocumentosEscolares(pacienteId);
+        DocumentoEscolarResponseDTO responseDTO = minioStorageService.listarDocumentosEscolares(pacienteId);
         if (ano != null) {
-            responseDTO = documentosEscolaresService.listarDocumentosEscolaresAno(pacienteId, ano);
+            responseDTO = minioStorageService.listarDocumentosEscolaresAno(pacienteId, ano);
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
 
-    @Operation(summary = "Retorna o histórico de documentos de um paciênte", description = "Recebe por parâmetro o UUID do paciênte")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Histórico dos documentos listados com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Paciênte não encontrado"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do minIO")
-    })
     @Override
     public ResponseEntity<DocumentoEscolarResponseDTO> historicoDocumentosEscolares(UUID pacienteId) {
-        DocumentoEscolarResponseDTO responseDTO = documentosEscolaresService.historicoDocumentosEscolares(pacienteId);
+        DocumentoEscolarResponseDTO responseDTO = minioStorageService.historicoDocumentosEscolares(pacienteId);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
 
-    @Operation(summary = "Retorna o documento de um paciênte para visualizar ou fazer download", description = "Recebe por parâmetro o UUID do paciênte e o nome do documento")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Documento encontrado com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Paciênte não encontrado ou documento não encontrado"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do minIO")
-    })
     @Override
     public ResponseEntity<byte[]> visualizarDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
-        byte[] response = documentosEscolaresService.visualizarDocumentoEscolar(pacienteId, nomeArquivo);
+        byte[] response = minioStorageService.visualizarDocumentoEscolar(pacienteId, nomeArquivo);
 
         MediaType contentType = getContentType(nomeArquivo);
 
@@ -85,15 +58,16 @@ public class DocumentosEscolaresControllerImp implements DocumentosEscolaresCont
                 .body(response);
     }
 
-    @Operation(summary = "Remove documento de um paciênte", description = "Recebe por parâmetro o UUID do paciênte e o nome do documento")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Documento removido com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Paciênte não encontrado ou documento não encontrado"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do minIO")
-    })
     @Override
-    public ResponseEntity<Void> deletarDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
-        documentosEscolaresService.deletarDocumentoEscolar(pacienteId, nomeArquivo);
+    public ResponseEntity<Void> atualizarDocumentoEscolar(DocumentoEscolarUpdateRequestDTO dto, MultipartFile arquivo) {
+        minioStorageService.atualizarDocumento(dto, arquivo);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> removerDocumentoEscolar(UUID pacienteId, String nomeArquivo) {
+        minioStorageService.deletarDocumentoEscolar(pacienteId, nomeArquivo);
 
         return ResponseEntity.noContent().build();
     }
