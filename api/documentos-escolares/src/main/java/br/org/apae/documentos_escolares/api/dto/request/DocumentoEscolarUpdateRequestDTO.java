426a172a197ee5389658eac4218e8b5e2e1c9589
ramon <ramon.bento.oliver@gmail.com>
2025-05-21 15:03:39 -0300

    chore[api]: adiciona novoNome

--- api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/dto/request/DocumentoEscolarUpdateRequestDTO.java
+++ api/documentos-escolares/src/main/java/br/org/apae/documentos_escolares/api/dto/request/DocumentoEscolarUpdateRequestDTO.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
-public record DocumentoEscolarUpdateRequestDTO(@NotNull(message = "O ID é obrigatório.") UUID pacienteId,
-                                               @NotBlank(message = "O ano é obrigatório") Integer ano,
-                                               @NotBlank(message = "O nome do documento é obrigatório") String documentoNome) {
-}
+public record DocumentoEscolarUpdateRequestDTO(
+        @NotNull(message = "O ID é obrigatório.") UUID pacienteId,
+        @NotNull(message = "O ano é obrigatório") Integer ano,
+        @NotBlank(message = "O nome atual do documento é obrigatório") String documentoNome,
+        @NotBlank(message = "O novo nome do documento é obrigatório") String novoNome
+) {}
