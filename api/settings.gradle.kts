cf3c03b0875064a57b016a6d6b8659c5ce516b62
dinizhugo <hugodiniztech01@gmail.com>
2025-05-30 14:16:25 -0300

    feat[gradle]: Adicionando o pacote auth-service no gradle (#9)

--- api/settings.gradle.kts
+++ api/settings.gradle.kts
@@ -4,4 +4,9 @@ plugins {
 }
 
 rootProject.name = "api"
-include("demo", "documentos-digitalizados", "documentos-medicos-digitalizados", "documentos-pessoais-digitalizados")
+include(
+    "demo", 
+    "documentos-digitalizados", 
+    "documentos-medicos-digitalizados", 
+    "documentos-pessoais-digitalizados",
+    "auth-service")
