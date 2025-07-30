81bd5bf84cd0252729a701949afcd995cc760f0a
kaik.alves.ext <kaik.alves.ext@synchro.com.br>
2025-07-30 08:53:31 -0300

    chore: estiliza a tela principal para centralizar e estilizar form de login

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,7 +1,10 @@
 import ButtonWrapper from './components/ButtonWrapper';
+import LoginForm from './components/LoginForm';
 
 export default function Home() {
   return (
-      <ButtonWrapper />
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-purple-200 p-4 font-sans">
+        <LoginForm />
+    </div>
   );
 }
