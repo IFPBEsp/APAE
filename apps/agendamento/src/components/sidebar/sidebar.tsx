44fcb0340a99782a80aa107d4bbbdff1e6af32aa
Raykkoner Dujhkkovick Silva de Farias <raykkonerd@gmail.com>
2025-07-31 21:46:52 +0000

    feat: adição de logo e botões de telas em sidebar

--- apps/agendamento/src/components/sidebar/sidebar.tsx
+++ apps/agendamento/src/components/sidebar/sidebar.tsx
@@ -5,20 +5,33 @@ import {
     SidebarGroup,
     SidebarHeader,
 } from "@/components/ui/sidebar";
-import { HomeIcon } from '@primer/octicons-react';
+import { HomeIcon, TasklistIcon, CalendarIcon } from '@primer/octicons-react';
 import styles from "./sidebar.module.css";
+import Image from "next/image";
 
 export function AppSidebar() {
     return (
-        <Sidebar>
-            <SidebarHeader>
-                <div>Título</div>
+        <Sidebar className={styles.sidebar}>
+            <SidebarHeader className={styles.header}>
+                <Image className={styles.logo} src={require("../../assets/APAE-logo.svg")} width={35} height={35} alt="Logo da APAE" />
+                <div>
+                    <div className={styles.title}>APAE</div>
+                    <div className={styles.subtitle}>Agendamento</div>
+                </div>
             </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup className={styles.listItem}>
-                    <HomeIcon size={20}/>
-                    <span>Dashboard</span>
-                </SidebarGroup>
+                <button className={styles.listItem}>
+                    <HomeIcon size={20} />
+                    <span>Tela inicial</span>
+                </button>
+                <button className={styles.listItem}>
+                    <TasklistIcon size={20} />
+                    <span>Todos os agendamentos</span>
+                </button>
+                <button className={styles.listItem}>
+                    <CalendarIcon size={20} />
+                    <span>Calendário</span>
+                </button>
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
