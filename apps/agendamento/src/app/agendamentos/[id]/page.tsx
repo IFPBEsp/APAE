2087c8caed5c46772b61cd844f6ae32e899eec31
lucasgomes14 <lucazmatehus14@gmail.com>
2025-08-03 18:55:01 -0300

    chore: avatar shadcn

--- apps/agendamento/src/app/agendamentos/[id]/page.tsx
+++ apps/agendamento/src/app/agendamentos/[id]/page.tsx
@@ -1,4 +1,16 @@
-import Avatar from "@/components/ui/avatar";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+
+// export default function Avatar() {
+//     return(
+//         <div className={"bg-gray-200 rounded-full p-5 flex items-center justify-center"}>
+//             <svg className={"w-12 h-12 md:w-25 md:h-25"} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+//                 <path
+//                     d="m 8 1 c -1.65625 0 -3 1.34375 -3 3 s 1.34375 3 3 3 s 3 -1.34375 3 -3 s -1.34375 -3 -3 -3 z m -1.5 7 c -2.492188 0 -4.5 2.007812 -4.5 4.5 v 0.5 c 0 1.109375 0.890625 2 2 2 h 8 c 1.109375 0 2 -0.890625 2 -2 v -0.5 c 0 -2.492188 -2.007812 -4.5 -4.5 -4.5 z m 0 0"
+//                     fill="#2e3436"/>
+//             </svg>
+//         </div>
+//     );
+// }
 
 interface PageProps {
     params: {
@@ -22,6 +34,7 @@ export default async function VisualizarAgendamento({ params } : PageProps) {
 
     const agendamento = {
         id,
+        imagem: "adfaf",
         nome: "Lucas Matheus Gomes de Lima",
         consulta: "pendente",
         agendamentoMarcado : {
@@ -67,13 +80,20 @@ export default async function VisualizarAgendamento({ params } : PageProps) {
         <div className={"mt-20 px-4 w-full"}>
             <div className={"flex flex-row items-center justify-between"}>
                 <div className={"flex items-center"}>
-                    <Avatar />
-                    <h1 className={"ml-10 text-[#0D4F97] text-xl md:text-2xl font-bold mr-5"}>{agendamento.nome}</h1>
-                </div>
-                <div>
-                    <p className={`${statusInfo.class} text-white py-1 px-2 rounded-md text-xs cursor-pointer font-medium text-center mr-3`}>{statusInfo.text}</p>
+                    <div className={"bg-gray-200 rounded-full w-17 h-17 md:w-23 md:h-23"}>
+                        <Avatar className={"p-5 w-17 h-17 md:w-23 md:h-23"}>
+                            <AvatarImage src="https://cdn-icons-png.flaticon.com/512/266/266033.png" alt="avatar"/>
+                            <AvatarFallback>{agendamento.nome.at(0)}</AvatarFallback>
+                        </Avatar>
+                    </div>
+                        <h1 className={"ml-10 text-[#0D4F97] text-xl md:text-2xl font-bold mr-5"}>{agendamento.nome}</h1>
+                    </div>
+                    <div>
+                        <p className={`${statusInfo.class} text-white py-1 px-2 rounded-md text-xs cursor-pointer font-medium text-center mr-3`}>{statusInfo.text}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
+            );
+            }
+
+            //
--- apps/agendamento/src/components/ui/avatar.tsx
+++ apps/agendamento/src/components/ui/avatar.tsx
@@ -1,11 +1,53 @@
-export default function Avatar() {
-    return(
-        <div className={"bg-gray-200 rounded-full p-5 flex items-center justify-center"}>
-            <svg className={"w-12 h-12 md:w-25 md:h-25"} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="m 8 1 c -1.65625 0 -3 1.34375 -3 3 s 1.34375 3 3 3 s 3 -1.34375 3 -3 s -1.34375 -3 -3 -3 z m -1.5 7 c -2.492188 0 -4.5 2.007812 -4.5 4.5 v 0.5 c 0 1.109375 0.890625 2 2 2 h 8 c 1.109375 0 2 -0.890625 2 -2 v -0.5 c 0 -2.492188 -2.007812 -4.5 -4.5 -4.5 z m 0 0"
-                    fill="#2e3436"/>
-            </svg>
-        </div>
-    );
+"use client"
+
+import * as React from "react"
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
+
+import { cn } from "@/lib/utils"
+
+function Avatar({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
+  return (
+    <AvatarPrimitive.Root
+      data-slot="avatar"
+      className={cn(
+        "relative flex size-8 shrink-0 overflow-hidden rounded-full",
+        className
+      )}
+      {...props}
+    />
+  )
 }
+
+function AvatarImage({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+  return (
+    <AvatarPrimitive.Image
+      data-slot="avatar-image"
+      className={cn("aspect-square size-full", className)}
+      {...props}
+    />
+  )
+}
+
+function AvatarFallback({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
+  return (
+    <AvatarPrimitive.Fallback
+      data-slot="avatar-fallback"
+      className={cn(
+        "bg-muted flex size-full items-center justify-center rounded-full",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Avatar, AvatarImage, AvatarFallback }
