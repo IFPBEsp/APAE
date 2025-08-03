0a2c05d54c1d7a1d86749e26bb4b8f68dfbfba13
anielmelo <anielcontato@gmail.com>
2025-08-03 18:43:51 -0300

    feat[api]: adiciona configuração inicial do microserviço de profissional da saúde

---
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/domain/repository/ProfissionalSaudeRepository.java
@@ -0,0 +1,23 @@
+package br.org.apae.profissional_da_saude.domain.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import br.org.apae.profissional_da_saude.domain.model.ProfissionalSaude;
+
+public interface ProfissionalSaudeRepository {
+  ProfissionalSaude save(ProfissionalSaude profissionalSaude);
+
+  Page<ProfissionalSaude> findAll(Pageable pageable);
+  
+  //TODO
+  ProfissionalSaude findById(UUID id);
+  
+  //TODO
+  ProfissionalSaude update(ProfissionalSaude profissionalSaude);
+  
+  //TODO
+  void deleteById(UUID id);
+}
