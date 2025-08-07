496d9900e1a00b32e468142cb8c4beb1a2731b8c
isnael07 <isnaellima24@gmail.com>
2025-08-07 19:07:56 -0300

    feat[service]: adiciona validações e exceções nos metodos save e delete

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/application/service/ProfissionalSaudeService.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/application/service/ProfissionalSaudeService.java
@@ -2,6 +2,8 @@ package br.org.apae.profissional_da_saude.application.service;
 
 import br.org.apae.profissional_da_saude.api.dto.ProfissionalSaudeCreateDTO;
 import br.org.apae.profissional_da_saude.api.dto.ProfissionalSaudeResponseDTO;
+import br.org.apae.profissional_da_saude.domain.exception.EntidadeNaoEncontradaException;
+import br.org.apae.profissional_da_saude.domain.exception.ValidacaoNegocioException;
 import br.org.apae.profissional_da_saude.domain.model.ProfissionalSaude;
 import br.org.apae.profissional_da_saude.domain.repository.ProfissionalSaudeRepository;
 import br.org.apae.profissional_da_saude.infrastructure.persistency.mapper.ProfissionalSaudeMapper;
@@ -25,6 +27,13 @@ public class ProfissionalSaudeService {
   }
 
   public ProfissionalSaudeResponseDTO save(ProfissionalSaudeCreateDTO dto) {
+    if (repository.existsByDocProfissional(dto.getDocProfissional())) {
+      throw new ValidacaoNegocioException("Documento profissional já cadastrado.");
+    }
+
+    if (repository.existsByEmail(dto.getEmail())) {
+      throw new ValidacaoNegocioException("E-mail já cadastrado.");
+    }
     ProfissionalSaude domain = ProfissionalSaudeMapper.toDomain(dto);
     ProfissionalSaude saved = repository.save(domain);
     return ProfissionalSaudeMapper.toResponseDTO(saved);
@@ -36,10 +45,14 @@ public class ProfissionalSaudeService {
   }
 
   public void delete(UUID id){
+    repository.findById(id).orElseThrow(
+            () -> new EntidadeNaoEncontradaException("Profissional não encontrado"));
+
     repository.deleteById(id);
   }
 
   public Optional<ProfissionalSaudeResponseDTO> findById(UUID id){
     return repository.findById(id).map(ProfissionalSaudeMapper::toResponseDTO);
   }
+
 }
