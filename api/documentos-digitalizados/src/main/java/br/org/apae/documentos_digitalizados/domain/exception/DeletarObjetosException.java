ccff6993800bc1246f1c778f8bb1e5f1e9caba42
dinizhugo <hugodiniztech01@gmail.com>
2025-08-09 10:36:31 -0300

    remove: Removendo as outras api's de documentos digitalizados que não serão mais necessarias (#99)

--- api/documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/DeletarObjetosException.java
+++
@@ -1,7 +0,0 @@
-package br.org.apae.documentos_digitalizados.domain.exception;
-
-public class DeletarObjetosException extends RuntimeException {
-    public DeletarObjetosException(String message) {
-        super(message);
-    }
-}
--- api/documentos-digitalizados/src/main/java/br/org/apae/documentos_digitalizados/domain/exception/DiretorioException.java
+++
@@ -1,7 +0,0 @@
-package br.org.apae.documentos_digitalizados.domain.exception;
-
-public class DiretorioException extends RuntimeException {
-    public DiretorioException(String message) {
-        super(message);
-    }
-}
