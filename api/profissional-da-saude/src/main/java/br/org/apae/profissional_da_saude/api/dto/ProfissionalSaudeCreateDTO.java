ebd931b29cac74e43868f4d3d509e609e1f67950
isnael07 <isnaellima24@gmail.com>
2025-08-10 22:14:50 -0300

    feat[repository]: adiciona anotações para validação

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/dto/ProfissionalSaudeCreateDTO.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/dto/ProfissionalSaudeCreateDTO.java
@@ -2,63 +2,34 @@ package br.org.apae.profissional_da_saude.api.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 public class ProfissionalSaudeCreateDTO {
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String areaDaSaude;
 
     @NotBlank
+    @Pattern(regexp = "^$|\\d{10,11}", message = "Telefone inválido")
     private String telefone;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String docProfissional;
 
-    @Email
+    @Email(message = "E-mail inválido")
     @NotBlank
+    @Size(max = 254)
     private String email;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String nome;
 
-    public String getAreaDaSaude() {
-        return areaDaSaude;
-    }
-
-    public void setAreaDaSaude(String areaDaSaude) {
-        this.areaDaSaude = areaDaSaude;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getDocProfissional() {
-        return docProfissional;
-    }
-
-    public void setDocProfissional(String docProfissional) {
-        this.docProfissional = docProfissional;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/domain/model/ProfissionalSaude.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/domain/model/ProfissionalSaude.java
@@ -1,25 +1,18 @@
 package br.org.apae.profissional_da_saude.domain.model;
 
-import br.org.apae.profissional_da_saude.domain.exception.DadosInvalidosException;
-import br.org.apae.profissional_da_saude.domain.exception.ValidacaoNegocioException;
-
 import java.util.UUID;
-import java.util.regex.Pattern;
+
 
 public class ProfissionalSaude {
   private UUID id;
-  private final String areaDaSaude;
-  private final String telefone;
-  private final String docProfissional;
-  private final String email;
-  private final String nome;
-
-
-  private static final Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+  private  String areaDaSaude;
+  private  String telefone;
+  private  String docProfissional;
+  private  String email;
+  private  String nome;
 
   public ProfissionalSaude(UUID id, String areaDaSaude, String telefone, String docProfissional, String email,
       String nome) {
-    validar(areaDaSaude, telefone, docProfissional, email, nome);
     this.id = id;
     this.areaDaSaude = areaDaSaude;
     this.telefone = telefone;
@@ -30,7 +23,6 @@ public class ProfissionalSaude {
 
   public ProfissionalSaude(String areaDaSaude, String telefone, String docProfissional, String email,
       String nome) {
-    validar(areaDaSaude, telefone, docProfissional, email, nome);
     this.areaDaSaude = areaDaSaude;
     this.telefone = telefone;
     this.docProfissional = docProfissional;
@@ -38,23 +30,6 @@ public class ProfissionalSaude {
     this.nome = nome;
   }
 
-  private void validar(String areaDaSaude, String telefone, String docProfissional, String email, String nome) {
-    if (areaDaSaude == null || areaDaSaude.trim().isEmpty()) {
-      throw new ValidacaoNegocioException("Área da saúde é obrigatória.");
-    }
-    if (telefone == null || telefone.trim().isEmpty()) {
-      throw new DadosInvalidosException("Telefone é obrigatório.");
-    }
-    if (docProfissional == null || docProfissional.trim().isEmpty()) {
-      throw new DadosInvalidosException("Documento profissional é obrigatório.");
-    }
-    if (email == null || email.trim().isEmpty() || !EMAIL_REGEX.matcher(email).matches()) {
-      throw new DadosInvalidosException("E-mail inválido.");
-    }
-    if (nome == null || nome.trim().isEmpty()) {
-      throw new DadosInvalidosException("Nome é obrigatório.");
-    }
-  }
   public UUID getId() {
     return id;
   }
@@ -77,5 +52,29 @@ public class ProfissionalSaude {
 
   public String getNome() {
     return nome;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public void setAreaDaSaude(String areaDaSaude) {
+    this.areaDaSaude = areaDaSaude;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public void setDocProfissional(String docProfissional) {
+    this.docProfissional = docProfissional;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
   }
 }
