bb8a1fbb271c9ce52cc9ad615d656377ba386b3b
kaik.alves.ext <kaik.alves.ext@synchro.com.br>
2025-08-11 20:23:52 -0300

    chore: adiciona imagem de fundo

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -3,7 +3,10 @@ import LoginForm from './components/LoginForm';
 
 export default function Home() {
   return (
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-purple-200 p-4 font-sans">
+      <div 
+        className="flex items-center justify-center min-h-screen p-4 font-sans bg-cover bg-center"
+        style={{ backgroundImage: 'url("/apae.svg")' }}
+      >
         <LoginForm />
     </div>
   );
