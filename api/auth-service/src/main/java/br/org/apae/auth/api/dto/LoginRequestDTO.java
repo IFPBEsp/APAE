81250de24e3e21e4d7f0c636e67afffb9ff33293
dinizhugo <hugodiniztech01@gmail.com>
2025-08-11 16:34:39 -0300

    chore: Adicionando métodos de validação nos atributos nos DTOS (#148)

--- api/auth-service/src/main/java/br/org/apae/auth/api/dto/LoginRequestDTO.java
+++ api/auth-service/src/main/java/br/org/apae/auth/api/dto/LoginRequestDTO.java
@@ -2,5 +2,22 @@ package br.org.apae.auth.api.dto;
 
 public record LoginRequestDTO(
     String username,
-    String password) {
+    String password) 
+{
+    public void validateAttributes() {
+        validateUsername();
+        validatePasswordLength();
+    }
+
+    private void validateUsername() {
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("Username cannot be null or blank");
+        }
+    }
+
+    private void validatePasswordLength() {
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be null or blank");
+        }
+    }
 }
