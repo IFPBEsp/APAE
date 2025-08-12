8a2deeaebe426a6a5c7b269cc638a2dc50bb3884
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2025-08-12 15:01:05 -0300

    fix: adicao das tags na classe entity de paciente

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/entity/PacienteEntity.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/entity/PacienteEntity.java
@@ -1,36 +1,68 @@
 package br.org.apae.profissional_da_saude.infrastructure.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
-@Table(name = "pacientes")
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
+@Table(name = "pacientes_temp")
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PacienteEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
+
+    @Column(name = "email", nullable = false, length = 150)
     private String email;
+
+    @Column(name = "telefone", nullable = false, length = 15)
     private String telefone;
+
+    @Column(name = "data_nascimento", nullable = false)
     private LocalDate dateNascimento;
+
+    @Column(name = "cpf", nullable = false, length = 11)
     private String cpf;
+
+    @Column(name = "rg", nullable = false, length = 20)
     private String rg;
+
+    @Column(name = "endereco", nullable = false, length = 255)
     private String endereco;
+
+    @Column(name = "bairro", nullable = false, length = 100)
     private String bairro;
+
+    @Column(name = "cidade", nullable = false, length = 100)
     private String cidade;
+
+    @Column(name = "estado", nullable = false, length = 2)
     private String estado;
+
+    @Column(name = "cep", nullable = false, length = 8)
     private String cep;
+
+    @CreationTimestamp
+    @Column(name = "data_criacao", updatable = false)
     private LocalDateTime dataCriacao;
 }
