09d3820c89bd533a65b293bf4190bfa19bb51075
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2025-08-12 15:03:23 -0300

    feat: criacao do endpoint de criacao de pacientes

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/controller/PacienteController.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/controller/PacienteController.java
@@ -1,13 +1,18 @@
 package br.org.apae.profissional_da_saude.api.controller;
 
 
-import br.org.apae.profissional_da_saude.api.dto.PacienteResposeDto;
+import br.org.apae.profissional_da_saude.api.dto.PacienteCreateDTO;
+import br.org.apae.profissional_da_saude.api.dto.PacienteResposeDTO;
 import br.org.apae.profissional_da_saude.application.service.PacienteService;
+import jakarta.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,14 +29,17 @@ public class PacienteController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PacienteResposeDto> findById(@PathVariable UUID id) {
+    public ResponseEntity<PacienteResposeDTO> findById(@PathVariable UUID id) {
         return ResponseEntity.ok(this.service.findById(id));
     }
 
     @GetMapping
-    public ResponseEntity<Page<PacienteResposeDto>> findByAll(Pageable pageable) {
+    public ResponseEntity<Page<PacienteResposeDTO>> findByAll(Pageable pageable) {
         return ResponseEntity.ok(this.service.findAll(pageable));
     }
 
-
+    @PostMapping("/create")
+    public ResponseEntity<PacienteResposeDTO> create (@RequestBody @Valid PacienteCreateDTO dto) {
+        return ResponseEntity.ok(service.create(dto));
+    }
 }
