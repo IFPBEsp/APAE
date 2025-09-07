8c3c36e0312ad0ea56d391e991b16f129d2b739d
Pedro Henrique Gregório <pedrosgregorio@gmail.com>
2025-09-07 18:00:23 -0300

    fix: remove files

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,13 +1,10 @@
-import ButtonWrapper from './components/ButtonWrapper';
-import LoginForm from './components/LoginForm';
-
 export default function Home() {
   return (
-      <div 
-        className="flex items-center justify-center min-h-screen p-4 font-sans bg-cover bg-center"
-        style={{ backgroundImage: 'url("/apae.svg")' }}
-      >
-        <LoginForm />
+    <div
+      className="flex items-center justify-center min-h-screen p-4 font-sans bg-cover bg-center"
+      style={{ backgroundImage: 'url("/apae.svg")' }}
+    >
+      <h1>OI</h1>
     </div>
   );
 }
