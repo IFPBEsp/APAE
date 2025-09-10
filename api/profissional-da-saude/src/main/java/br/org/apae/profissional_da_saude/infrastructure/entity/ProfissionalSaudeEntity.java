a8504fbd422fd748877a68a10a09df518cbcd0e2
isnael07 <isnaellima24@gmail.com>
2025-09-10 19:11:30 -0300

    refactor[entity]: extrai campos de endereço para classe EnderecoEntity e atualiza ProfissionalSaudeEntity

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/entity/ProfissionalSaudeEntity.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/entity/ProfissionalSaudeEntity.java
@@ -22,13 +22,9 @@ public class ProfissionalSaudeEntity {
     private String docProfissional;
     private String email;
     private String nome;
-
     private  String rg;
-    private  String estado;
-    private  String cidade;
-    private  String bairro;
-    private  String rua;
-    private  String numero;
-    private  String cep;
-    private  String complemento;
+
+    @Embedded
+    private EnderecoEntity endereco;
+
 }
