128cd1d0f7a0175dd8b3e2b5b77402b8b63b3fc4
isnael07 <isnaellima24@gmail.com>
2025-09-23 22:10:26 -0300

    feat[mapper]: adiciona o mapper de Area de saude.

---
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/persistency/mapper/AreaSaudeMapper.java
@@ -0,0 +1,37 @@
+package br.org.apae.profissional_da_saude.infrastructure.persistency.mapper;
+
+import br.org.apae.profissional_da_saude.api.dto.AreaSaudeCreateDTO;
+import br.org.apae.profissional_da_saude.api.dto.AreaSaudeResponseDTO;
+import br.org.apae.profissional_da_saude.domain.model.AreaSaude;
+import br.org.apae.profissional_da_saude.infrastructure.entity.AreaSaudeEntity;
+
+public final class AreaSaudeMapper {
+
+    private AreaSaudeMapper() {
+    }
+
+    public static AreaSaudeEntity toEntity(AreaSaude model){
+        return AreaSaudeEntity.builder()
+                .id(model.getId())
+                .area(model.getArea())
+            .build();
+    }
+    public static AreaSaude toModel(AreaSaudeEntity entity){
+        return new AreaSaude(
+                entity.getId(),
+                entity.getArea()
+        );
+    }
+
+    public static AreaSaude toDomain(AreaSaudeCreateDTO dto){
+        return new AreaSaude(
+                dto.getArea()
+        );
+    }
+    public static AreaSaudeResponseDTO toResponseDTO(AreaSaude model){
+        AreaSaudeResponseDTO area = new AreaSaudeResponseDTO();
+        area.setId(model.getId());
+        area.setArea(model.getArea());
+        return area;
+    }
+}
