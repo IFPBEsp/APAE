f92f9e0064eed55d1d759382acccc941e7b9997b
isnael07 <isnaellima24@gmail.com>
2025-09-23 22:10:10 -0300

    feat[entity]: adiciona a entidade de Area de saude.

---
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/entity/AreaSaudeEntity.java
@@ -0,0 +1,20 @@
+package br.org.apae.profissional_da_saude.infrastructure.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "area_saude")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AreaSaudeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    private String area;
+
+}
