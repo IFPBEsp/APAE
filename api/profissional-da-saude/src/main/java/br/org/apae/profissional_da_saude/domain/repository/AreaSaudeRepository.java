29b7e93d46c7d0d7807f59c6a9d39642995b6d32
isnael07 <isnaellima24@gmail.com>
2025-09-23 22:12:18 -0300

    feat[Repository]: adiciona o repository de Area de saude sem a implementação.

---
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/domain/repository/AreaSaudeRepository.java
@@ -0,0 +1,20 @@
+package br.org.apae.profissional_da_saude.domain.repository;
+
+import br.org.apae.profissional_da_saude.domain.model.AreaSaude;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface AreaSaudeRepository {
+
+    AreaSaude save(AreaSaude areaSaude);
+
+    Page<AreaSaude> findAll(Pageable pageable);
+
+    Optional<AreaSaude> findById(Integer id);
+
+    AreaSaude update(AreaSaude areaSaude);
+
+    void deleteById(Integer id);
+}
---
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/infrastructure/persistency/jpa/AreaSaudeRepositoryJpa.java
@@ -0,0 +1,7 @@
+package br.org.apae.profissional_da_saude.infrastructure.persistency.jpa;
+
+import br.org.apae.profissional_da_saude.infrastructure.entity.AreaSaudeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaSaudeRepositoryJpa extends JpaRepository<AreaSaudeEntity, Integer> {
+}
