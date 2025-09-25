f1f850dc73f68690ce0f54427afd2585be59174f
isnael07 <isnaellima24@gmail.com>
2025-09-25 15:12:20 -0300

    chore[dto]: adiciona validação de caracteres nos DTOs de Area de saude.

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/dto/AreaSaudeCreateDTO.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/dto/AreaSaudeCreateDTO.java
@@ -1,6 +1,7 @@
 package br.org.apae.profissional_da_saude.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,5 +12,7 @@ public class AreaSaudeCreateDTO {
 
     @NotBlank
     @Size(min = 3, max = 100)
+    @Pattern(regexp = "^[A-Za-zÀ-ÿ ]+$",
+            message = "A área de saúde deve conter apenas letras e espaços")
     private String area;
 }
--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/dto/AreaSaudeUpdateDTO.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/api/dto/AreaSaudeUpdateDTO.java
@@ -1,6 +1,7 @@
 package br.org.apae.profissional_da_saude.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
@@ -8,5 +9,7 @@ import lombok.Getter;
 public class AreaSaudeUpdateDTO {
     @NotBlank
     @Size(min = 3, max = 100)
+    @Pattern(regexp = "^[A-Za-zÀ-ÿ ]+$",
+            message = "A área de saúde deve conter apenas letras e espaços")
     private String area;
 }
