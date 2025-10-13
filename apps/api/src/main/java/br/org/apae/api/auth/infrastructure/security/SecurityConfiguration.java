d6efe43689e6b9ac6429e0f5f5ba676d00edc4d8
anielmelo <anielcontato@gmail.com>
2025-10-13 11:03:49 -0300

    fix(auth): corrige permissões na configuração de segurança

--- apps/api/src/main/java/br/org/apae/api/auth/infrastructure/security/SecurityConfiguration.java
+++ apps/api/src/main/java/br/org/apae/api/auth/infrastructure/security/SecurityConfiguration.java
@@ -27,7 +27,6 @@ public class SecurityConfiguration {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers("/auth/**").permitAll()
-            .requestMatchers("/professionals/**").permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
