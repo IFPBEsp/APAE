cf76565ecce844cead57be5fb5c0010fd137dbb0
anielmelo <anielcontato@gmail.com>
2025-10-19 19:00:40 -0300

    fix(controllers): ajusta contratos

--- apps/api/src/main/java/br/org/apae/api/controllers/patient/VaccineControllerImpl.java
+++ apps/api/src/main/java/br/org/apae/api/controllers/patient/VaccineControllerImpl.java
@@ -1,14 +1,12 @@
 package br.org.apae.api.controllers.patient;
 
 import br.org.apae.api.patient.application.internal.VaccineService;
-import br.org.apae.api.patient.domain.model.Vaccine;
-import br.org.apae.api.common.dto.vaccine.create.CreateVaccineDTO;
-import br.org.apae.api.common.dto.vaccine.response.ResponseVaccineDTO;
+import br.org.apae.api.common.dto.patient.request.vaccine.CreateVaccineDTO;
+import br.org.apae.api.common.dto.patient.response.vaccine.VaccineResponseDTO;
 import br.org.apae.api.patient.interfaces.controllers.VaccineController;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,45 +22,38 @@ public class VaccineControllerImpl implements VaccineController {
     }
 
     @Override
-    public ResponseEntity<ResponseVaccineDTO> createVaccine(CreateVaccineDTO vaccineDTO) {
-        Vaccine createdVaccine = vaccineService.create(vaccineDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(toResponseDTO(createdVaccine));
+    public ResponseEntity<VaccineResponseDTO> createVaccine(CreateVaccineDTO vaccineDTO) {
+        VaccineResponseDTO createdVaccine = vaccineService.createVaccine(vaccineDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdVaccine);
     }
 
     @Override
-    public ResponseEntity<ResponseVaccineDTO> findById(UUID id) {
-        Vaccine vaccine = vaccineService.findById(id);
-        return ResponseEntity.ok(toResponseDTO(vaccine));
+    public ResponseEntity<VaccineResponseDTO> findById(UUID id) {
+        VaccineResponseDTO vaccine = vaccineService.findById(id);
+        return ResponseEntity.ok(vaccine);
     }
 
     @Override
-    public ResponseEntity<List<ResponseVaccineDTO>> findAll() {
-        List<Vaccine> vaccines = vaccineService.findAll();
-        List<ResponseVaccineDTO> responseDTOs = vaccines.stream()
-                .map(this::toResponseDTO)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(responseDTOs);
+    public ResponseEntity<List<VaccineResponseDTO>> findAll() {
+        List<VaccineResponseDTO> vaccines = vaccineService.findAll();
+        return ResponseEntity.ok(vaccines);
     }
 
     @Override
-    public ResponseEntity<ResponseVaccineDTO> findByName(String name) {
-        Vaccine vaccine = vaccineService.findByName(name);
-        return ResponseEntity.ok(toResponseDTO(vaccine));
+    public ResponseEntity<VaccineResponseDTO> findByName(String name) {
+        VaccineResponseDTO vaccine = vaccineService.findByName(name);
+        return ResponseEntity.ok(vaccine);
     }
 
     @Override
-    public ResponseEntity<ResponseVaccineDTO> updateVaccine(UUID id, CreateVaccineDTO vaccineDTO) {
-        Vaccine updatedVaccine = vaccineService.update(id, vaccineDTO);
-        return ResponseEntity.ok(toResponseDTO(updatedVaccine));
+    public ResponseEntity<VaccineResponseDTO> updateVaccine(UUID id, CreateVaccineDTO vaccineDTO) {
+        VaccineResponseDTO updatedVaccine = vaccineService.update(id, vaccineDTO);
+        return ResponseEntity.ok(updatedVaccine);
     }
 
     @Override
     public ResponseEntity<Void> deleteVaccine(UUID id) {
         vaccineService.delete(id);
         return ResponseEntity.noContent().build();
-    }
-
-    private ResponseVaccineDTO toResponseDTO(Vaccine vaccine) {
-        return new ResponseVaccineDTO(vaccine.getId(), vaccine.getName());
     }
 }
