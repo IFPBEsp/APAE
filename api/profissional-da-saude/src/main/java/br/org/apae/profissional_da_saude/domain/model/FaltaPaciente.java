75ed3c4b2f436e4cfc6a5c09faf6078bda4f303c
Cecilia-SGomes <cecilia.gomes@academico.ifpb.edu.br>
2025-10-26 16:32:41 -0300

    feat[model]: Cria classe FaltaPaciente

---
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/domain/model/FaltaPaciente.java
@@ -0,0 +1,110 @@
+package br.org.apae.profissional_da_saude.domain.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+public class FaltaPaciente {
+
+    private UUID id;
+
+    private LocalDate data;
+    private LocalTime hora;
+    private Boolean justificativa;
+    private String motivo;
+
+    private UUID fk_atendimento;
+    private UUID fk_profissional;
+    private UUID fk_cadastro_anual;
+
+    public FaltaPaciente(LocalDate data, LocalTime hora, Boolean justificativa, String motivo, UUID fk_atendimento,
+            UUID fk_profissional, UUID fk_cadastro_anual) {
+        this.data = data;
+        this.hora = hora;
+        this.justificativa = justificativa;
+        this.motivo = motivo;
+        this.fk_atendimento = fk_atendimento;
+        this.fk_profissional = fk_profissional;
+        this.fk_cadastro_anual = fk_cadastro_anual;
+    }
+
+    public FaltaPaciente(UUID id, LocalDate data, LocalTime hora, Boolean justificativa, String motivo,
+            UUID fk_atendimento, UUID fk_profissional, UUID fk_cadastro_anual) {
+        this.id = id;
+        this.data = data;
+        this.hora = hora;
+        this.justificativa = justificativa;
+        this.motivo = motivo;
+        this.fk_atendimento = fk_atendimento;
+        this.fk_profissional = fk_profissional;
+        this.fk_cadastro_anual = fk_cadastro_anual;
+    }
+    // Getters e Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public Boolean getJustificativa() {
+        return justificativa;
+    }
+
+    public void setJustificativa(Boolean justificativa) {
+        this.justificativa = justificativa;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    // Chaves Estrangeiras
+
+    public UUID getFkAtendimento() {
+        return fk_atendimento;
+    }
+
+    public void setFkAtendimento(UUID fk_atendimento) {
+        this.fk_atendimento = fk_atendimento;
+    }
+
+    public UUID getFkProfissional() {
+        return fk_profissional;
+    }
+
+    public void setFkProfissional(UUID fk_profissional) {
+        this.fk_profissional = fk_profissional;
+    }
+
+    public UUID getFkCadastroAnual() {
+        return fk_cadastro_anual;
+    }
+
+    public void setFkCadastroAnual(UUID fk_cadastro_anual) {
+        this.fk_cadastro_anual = fk_cadastro_anual;
+    }
+
+}
