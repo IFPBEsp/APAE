7b6f564ce4fa55a4b568706007657c7923efbd84
anielmelo <anielcontato@gmail.com>
2025-10-26 18:39:31 -0300

    refactor(address): ajusta importações e arquitetura de pastas

--- apps/api/src/main/java/br/org/apae/api/address/application/interfaces/AddressService.java
+++ apps/api/src/main/java/br/org/apae/api/address/application/interfaces/AddressService.java
@@ -1,8 +1,13 @@
 package br.org.apae.api.address.application.interfaces;
 
+import java.util.UUID;
+
 import br.org.apae.api.common.dto.address.AddressResponseDTO;
 import br.org.apae.api.common.dto.address.CreateAddressDTO;
+import br.org.apae.api.common.dto.address.UpdateAddressDTO;
 
 public interface AddressService {
   AddressResponseDTO createAddress(CreateAddressDTO createAddressDTO);
+
+  AddressResponseDTO updateAddress(UUID addressId, UpdateAddressDTO updateAddressDTO);
 }
--- apps/api/src/main/java/br/org/apae/api/address/application/internal/AddressServiceImpl.java
+++ apps/api/src/main/java/br/org/apae/api/address/application/internal/AddressServiceImpl.java
@@ -1,12 +1,20 @@
 package br.org.apae.api.address.application.internal;
 
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
 import br.org.apae.api.address.application.interfaces.AddressService;
 import br.org.apae.api.address.application.mapper.AddressMapper;
+import br.org.apae.api.address.domain.exceptions.AddressNotFoundException;
 import br.org.apae.api.address.domain.model.Address;
 import br.org.apae.api.address.domain.repository.AddressRepository;
 import br.org.apae.api.common.dto.address.AddressResponseDTO;
 import br.org.apae.api.common.dto.address.CreateAddressDTO;
+import br.org.apae.api.common.dto.address.UpdateAddressDTO;
+import jakarta.transaction.Transactional;
 
+@Service
 public class AddressServiceImpl implements AddressService {
   private final AddressRepository addressRepository;
   private final AddressMapper addressMapper;
@@ -22,5 +30,15 @@ public class AddressServiceImpl implements AddressService {
     Address addressCreated = this.addressRepository.save(address);
 
     return addressMapper.toResponseDTO(addressCreated);
+  }
+
+  @Override
+  @Transactional
+  public AddressResponseDTO updateAddress(UUID addressId, UpdateAddressDTO updateAddressDto) {
+    Address address = addressRepository.findById(addressId).orElseThrow(AddressNotFoundException::new);
+
+    Address addressUpdated = addressMapper.updateEntityFromDto(address, updateAddressDto);
+
+    return addressMapper.toResponseDTO(addressUpdated);
   }
 }
