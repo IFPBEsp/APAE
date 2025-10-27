23631bf6d5310955fcfb0b3093f26683a2c33031
Cecilia-SGomes <cecilia.gomes@academico.ifpb.edu.br>
2025-10-27 19:09:09 -0300

    refactor[application]: refatora exceções

--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/application/service/exceptions/FaltaDuplicadaException.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/application/service/exceptions/FaltaDuplicadaException.java
@@ -1,5 +1,8 @@
 package br.org.apae.profissional_da_saude.application.service.exceptions;
 
-public class FaltaDuplicadaException {
+public class FaltaDuplicadaException extends RuntimeException {
 
+    public FaltaDuplicadaException() {
+        super("Já existe uma falta registrada");
+    }
 }
--- api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/application/service/exceptions/PacienteNaoEncontradoException.java
+++ api/profissional-da-saude/src/main/java/br/org/apae/profissional_da_saude/application/service/exceptions/PacienteNaoEncontradoException.java
@@ -3,6 +3,6 @@ package br.org.apae.profissional_da_saude.application.service.exceptions;
 public class PacienteNaoEncontradoException extends RuntimeException {
 
     public PacienteNaoEncontradoException() {
-        super("Já existe uma falta registrada");
+        super("Paciente não encontrado");
     }
 }
