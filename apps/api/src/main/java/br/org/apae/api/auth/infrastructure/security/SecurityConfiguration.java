c709caece6a3348db00190cd5e1a758a7c91438a
anielmelo <anielcontato@gmail.com>
2025-11-01 23:21:22 -0300

    refactor(patient): ajusta permissões

--- apps/api/src/main/java/br/org/apae/api/auth/infrastructure/security/SecurityConfiguration.java
+++ apps/api/src/main/java/br/org/apae/api/auth/infrastructure/security/SecurityConfiguration.java
@@ -27,7 +27,6 @@ public class SecurityConfiguration {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers("/auth/**").permitAll()
-            .requestMatchers("/patients/**").permitAll()
             .requestMatchers(
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
