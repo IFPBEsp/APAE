e9762903c04e33f90a5d23f54383ce72161cad00
anielmelo <anielcontato@gmail.com>
2025-11-01 22:42:06 -0300

    refactor(patient): corrige validação de dto para cadastro de paciente

--- apps/api/src/main/java/br/org/apae/api/common/dto/patient/request/patient/CreatePatientDTO.java
+++ apps/api/src/main/java/br/org/apae/api/common/dto/patient/request/patient/CreatePatientDTO.java
@@ -17,46 +17,47 @@ import jakarta.validation.constraints.Past;
 
 public record CreatePatientDTO(
 
-                @NotBlank(message = "O nome não pode estar em branco") String fullName,
+    @NotBlank(message = "O nome não pode estar em branco") String fullName,
 
-                @NotBlank(message = "A naturalidade não pode estar em branco") String nationality,
+    @NotBlank(message = "A naturalidade não pode estar em branco") String nationality,
 
-                @NotNull(message = "A data de nascimento não pode ser nula") @Past(message = "A data de nascimento deve ser uma data no passado") LocalDate birthDate,
+    @NotNull(message = "A data de nascimento não pode ser nula") @Past(message = "A data de nascimento deve ser uma data no passado") LocalDate birthDate,
 
-                @NotBlank(message = "O contato não pode estar em branco") String contact,
+    @NotBlank(message = "O contato não pode estar em branco") String contact,
 
-                @NotBlank(message = "O número da certidão de nascimento não pode estar em branco") String birthCertificateNumber,
+    @NotBlank(message = "O número da certidão de nascimento não pode estar em branco") String birthCertificateNumber,
 
-                @NotBlank(message = "O cartório não pode estar em branco") String registryOffice,
+    @NotBlank(message = "O cartório não pode estar em branco") String registryOffice,
 
-                @NotBlank(message = "O campo 'fls' não pode estar em branco") String fls,
+    @NotBlank(message = "O campo 'fls' não pode estar em branco") String fls,
 
-                @NotBlank(message = "O campo 'livro' não pode estar em branco") String book,
+    @NotBlank(message = "O campo 'livro' não pode estar em branco") String book,
 
-                @NotBlank(message = "O RG não pode estar em branco") String rg,
+    @NotBlank(message = "O RG não pode estar em branco") String rg,
 
-                @NotNull(message = "A data de emissão não pode ser nula") LocalDate issueDate,
+    @NotNull(message = "A data de emissão não pode ser nula") LocalDate issueDate,
 
-                @NotBlank(message = "O órgão emissor não pode estar em branco") String issuingAgency,
+    @NotBlank(message = "O órgão emissor não pode estar em branco") String issuingAgency,
 
-                @NotBlank(message = "O CPF não pode estar em branco") String cpf,
+    @NotBlank(message = "O CPF não pode estar em branco") String cpf,
 
-                @NotBlank(message = "O CNS não pode estar em branco") String cns,
+    @NotBlank(message = "O CNS não pode estar em branco") String cns,
 
-                @NotBlank(message = "O NIS não pode estar em branco") String nis,
+    @NotBlank(message = "O NIS não pode estar em branco") String nis,
 
-                @NotNull(message = "A data de cadastro não pode ser nula") LocalDate registrationDate,
+    @NotNull(message = "A data de cadastro não pode ser nula") LocalDate registrationDate,
 
-                @NotBlank(message = "As alergias não podem estar em branco") String allergies,
+    @NotBlank(message = "As alergias não podem estar em branco") String allergies,
 
-                @NotNull(message = "O campo 'é estudante' não pode ser nulo") boolean isStudent,
+    @NotNull(message = "O campo 'é estudante' não pode ser nulo") boolean isStudent,
 
-                @NotNull(message = "Os dados de endereço são obrigatórios") @Valid CreateAddressDTO address,
+    @NotNull(message = "Os dados de endereço são obrigatórios") @Valid CreateAddressDTO address,
 
-                @NotNull(message = "Os dados do responsável são obrigatórios") @Valid CreateGuardianDTO guardian,
+    @NotNull(message = "Os dados do responsável são obrigatórios") @Valid CreateGuardianDTO guardian,
 
-                @NotEmpty(message = "É necessário fornecer os dados de pelo menos um parente ou responsável legal") @Valid List<CreateParentDTO> parents,
+    @NotEmpty(message = "É necessário fornecer os dados de pelo menos um parente ou responsável legal") @Valid List<CreateParentDTO> parents,
 
-                @NotEmpty(message = "A lista de vacinas não pode estar vazia") Set<CreateVaccineDTO> vaccineNames,
-                @NotNull CreateAnnualRegistryDTO annualRegistry) {
+    @NotEmpty(message = "A lista de vacinas não pode estar vazia") Set<CreateVaccineDTO> vaccineNames,
+
+    @NotNull(message = "Cadastro anual é obrigatório") @Valid CreateAnnualRegistryDTO annualRegistry) {
 }
