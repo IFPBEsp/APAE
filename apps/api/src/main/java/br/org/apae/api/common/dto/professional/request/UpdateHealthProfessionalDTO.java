de283b6b98befbb5ee6218cb60e0a84e4787a566
isnael07 <isnaellima24@gmail.com>
2025-11-05 12:48:52 -0300

    chore[dto]: adiciona validações de tamanho e formato no update de Profissional de saúde e de endereço

--- apps/api/src/main/java/br/org/apae/api/common/dto/professional/request/UpdateHealthProfessionalDTO.java
+++ apps/api/src/main/java/br/org/apae/api/common/dto/professional/request/UpdateHealthProfessionalDTO.java
@@ -5,17 +5,33 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 
 public record UpdateHealthProfessionalDTO(
-    @NotBlank(message = "O setor de saúde é obrigatório.") String healthSector,
+        @NotBlank(message = "O setor de saúde é obrigatório.")
+        @Size(min = 3, max = 100)
+        String healthSector,
 
-    @NotBlank(message = "O número de telefone é obrigatório.") String phoneNumber,
+        @NotBlank(message = "O número de telefone é obrigatório.")
+        @Pattern(regexp = "^\\(\\d{2}\\) \\d{4,5}-\\d{4}$", message = "Telefone inválido")
+        String phoneNumber,
 
-    @NotBlank(message = "O documento profissional é obrigatório.") String professionalDocument,
+        @NotBlank(message = "O documento profissional é obrigatório.")
+        @Pattern(regexp = "^[A-Z]{2,8}\\s?\\d{1,6}(?:[/-][A-Z0-9]{1,5})?$", message = "Documento profissional inválido")
+        @Size(min = 3, max = 50)
+        String professionalDocument,
 
-    @Email(message = "O e-mail informado é inválido.") @NotBlank(message = "O e-mail é obrigatório.") String email,
+        @Email(message = "O e-mail informado é inválido.")
+        @NotBlank(message = "O e-mail é obrigatório.")
+        String email,
 
-    @NotBlank(message = "O nome é obrigatório.") String name,
+        @NotBlank(message = "O nome é obrigatório.")
+        @Size(min = 3, max = 100)
+        String name,
 
-    @NotBlank(message = "O documento de identidade é obrigatório.") String identityDocument,
+        @NotBlank(message = "O documento de identidade é obrigatório.")
+        @Pattern(
+                regexp = "^(\\d{7,9}|\\d{11}|\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2})$",
+                message = "Documento inválido"
+        )
+        String identityDocument,
 
-    @NotNull(message = "O endereço é obrigatório.") @Valid UpdateAddressDTO address) {
+        @NotNull(message = "O endereço é obrigatório.") @Valid UpdateAddressDTO address) {
 }
