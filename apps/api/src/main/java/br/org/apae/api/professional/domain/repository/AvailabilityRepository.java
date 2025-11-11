63df6434c159179b55d7e3772812f2cbfaa93f77
isnael07 <isnaellima24@gmail.com>
2025-11-10 21:15:16 -0300

    chore[repository]: corrige erro de digitação no nome do repository

--- apps/api/src/main/java/br/org/apae/api/professional/domain/repository/AvailabilityRepository.java
+++ apps/api/src/main/java/br/org/apae/api/professional/domain/repository/AvailabilityRepository.java
@@ -4,8 +4,8 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.org.apae.api.professional.domain.model.Avaliability;
+import br.org.apae.api.professional.domain.model.Availability;
 
-public interface AvaliabilityRepository extends JpaRepository<Avaliability, UUID> {
+public interface AvailabilityRepository extends JpaRepository<Availability, UUID> {
 
 }
