42cf80fcead955ccc5813375a878ce1802128c35
ismaellima4 <ismael.github@gmail.com>
2025-11-17 22:58:27 -0300

    refactor: add cors origin

--- apps/api/src/main/java/br/org/apae/api/controllers/appointment/AppointmentControllerImpl.java
+++ apps/api/src/main/java/br/org/apae/api/controllers/appointment/AppointmentControllerImpl.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.apae.api.appointment.application.interfaces.AppointmentApplicationService;
@@ -17,6 +18,7 @@ import br.org.apae.api.common.dto.appointment.response.appointment.*;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AppointmentControllerImpl implements AppointmentController {
 
   private final AppointmentApplicationService service;
