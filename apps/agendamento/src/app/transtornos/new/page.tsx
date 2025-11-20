f4573c3a92e4b8d35c0d326808e2e4af539e7ee9
Pedro Henrique <132232273+Pedroo722@users.noreply.github.com>
2025-11-20 20:56:24 +0000

    chore: adicione flex-1 na div pai das telas para corrigir a estilizacao das telas em desktop

--- apps/agendamento/src/app/transtornos/new/page.tsx
+++ apps/agendamento/src/app/transtornos/new/page.tsx
@@ -41,7 +41,7 @@ export default function NewTranstornoPage() {
   };
 
   return (
-    <div className="!bg-slate-100 min-h-screen">
+    <div className="flex-1 !bg-slate-100 min-h-screen">
       <main className="container mx-auto p-4 md:p-6">
         <div className="bg-white rounded-xl shadow-md border-2 p-6 mb-4">
           <Button 
