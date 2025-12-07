85cd83e2d67f18696fcee1e45da83ce14fdd80c8
hugo-diniz <hugo.diniz.ext@synchro.com.br>
2025-12-07 20:13:01 -0300

    chore: adiciona requestpart para retornar os documentos pessoais por tipo

--- apps/api/src/main/java/br/org/apae/api/controllers/patient/PatientDocumentsControllerImpl.java
+++ apps/api/src/main/java/br/org/apae/api/controllers/patient/PatientDocumentsControllerImpl.java
@@ -3,6 +3,7 @@ package br.org.apae.api.controllers.patient;
 import br.org.apae.api.common.dto.patient.response.documents.DocumentWithUrlResponseDTO;
 import br.org.apae.api.documents.application.interfaces.DocumentApplicationService;
 import br.org.apae.api.documents.domain.enums.DocumentCategory;
+import br.org.apae.api.documents.domain.enums.DocumentType;
 import br.org.apae.api.documents.interfaces.dto.DocumentDTO;
 import br.org.apae.api.documents.interfaces.dto.GetPresignedDocumentUrlArgsDTO;
 import br.org.apae.api.documents.interfaces.dto.ListDocumentsArgsDTO;
@@ -10,8 +11,11 @@ import br.org.apae.api.patient.application.interfaces.PatientDocumentsApplicatio
 import br.org.apae.api.patient.interfaces.controllers.PatientDocumentsController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Year;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.StreamSupport;
@@ -63,14 +67,34 @@ public class PatientDocumentsControllerImpl implements PatientDocumentsControlle
         }
     }
 
+    private List<DocumentWithUrlResponseDTO> findDocumentsByCategory(UUID ownerId, DocumentCategory category, String type) {
+        try {
+            Iterable<DocumentDTO> documents = this.documentService.listDocuments(
+                    ListDocumentsArgsDTO.builder()
+                            .owner(ownerId.toString())
+                            .category(category)
+                            .type(Optional.ofNullable(type).isPresent() ? DocumentType.valueOf(type) : null)
+                            .year(Year.now())
+                            .build()
+            );
+
+            return StreamSupport.stream(documents.spliterator(), false)
+                    .map(this::generatePresignedUrl)
+                    .filter(Objects::nonNull)
+                    .toList();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar documentos", e);
+        }
+    }
+
     @Override
     public ResponseEntity<List<DocumentWithUrlResponseDTO>> findMedicalDocuments(UUID id) {
         return ResponseEntity.ok(findDocumentsByCategory(id, DocumentCategory.MEDICAL));
     }
 
     @Override
-    public ResponseEntity<List<DocumentWithUrlResponseDTO>> findPersonalDocuments(UUID id) {
-        return ResponseEntity.ok(findDocumentsByCategory(id, DocumentCategory.PERSONAL));
+    public ResponseEntity<List<DocumentWithUrlResponseDTO>> findPersonalDocuments(UUID id, String type) {
+        return ResponseEntity.ok(findDocumentsByCategory(id, DocumentCategory.PERSONAL, type));
     }
 
     @Override
