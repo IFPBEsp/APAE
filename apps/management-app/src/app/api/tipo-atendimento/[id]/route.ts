1b030e81e050e8afa056d4ee1edc9862a2464358
hugo-diniz <hugo.diniz.ext@synchro.com.br>
2025-12-27 21:27:41 -0300

    feat: cira nova tela de tipo-atendimento (#430)

---
+++ apps/management-app/src/app/api/tipo-atendimento/[id]/route.ts
@@ -0,0 +1,44 @@
+import { createBaseApi } from "@/lib/axios";
+import { updateserviceTypeSchema } from "@/schemas/service-type-schemas";
+import { NextResponse } from "next/server";
+
+interface IParams {
+  params: Promise<{ id: string }>;
+}
+
+export async function GET(request: Request, { params }: IParams) {
+  try {
+    const { id } = await params; 
+    const api = await createBaseApi();
+    const { data } = await api.get(`/service-areas/${id}`);
+    return NextResponse.json(data);
+  } catch (error: any) {
+    return new NextResponse(JSON.stringify(error.response?.data), { status: 500 });
+  }
+}
+
+export async function PUT(request: Request, { params }: IParams) {
+  try {
+    const { id } = await params;
+    const body = await request.json();
+    const validation = updateserviceTypeSchema.safeParse(body);
+    if (!validation.success) return NextResponse.json({ errors: validation.error.flatten() }, { status: 400 });
+
+    const api = await createBaseApi();
+    const { data } = await api.put(`/service-areas/${id}`, validation.data);
+    return NextResponse.json(data);
+  } catch (error: any) {
+    return new NextResponse(JSON.stringify(error.response?.data), { status: 500 });
+  }
+}
+
+export async function DELETE(request: Request, { params }: IParams) {
+  try {
+    const { id } = await params;
+    const api = await createBaseApi();
+    const { data } = await api.delete(`/service-areas/${id}`);
+    return NextResponse.json(data);
+  } catch (error: any) {
+    return new NextResponse(JSON.stringify(error.response?.data), { status: 500 });
+  }
+}
