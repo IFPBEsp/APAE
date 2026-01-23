f670623cdab2315283d78b55ae7a8f451351afa8
ismaellima4 <ismael.github@gmail.com>
2026-01-23 18:32:18 -0300

    feat: add exception handler para erros de validacao

--- apps/api/src/main/java/br/org/apae/api/appointment/exceptions/AppointmentExceptionHandler.java
+++ apps/api/src/main/java/br/org/apae/api/appointment/exceptions/AppointmentExceptionHandler.java
@@ -3,6 +3,9 @@ package br.org.apae.api.appointment.exceptions;
 import br.org.apae.api.appointment.domain.exceptions.AppointmentAlreadyCancelledException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -36,7 +39,42 @@ public class AppointmentExceptionHandler {
   }
 
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpServletRequest request
+  ) {
+    String message = ex.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .findFirst()
+        .map(FieldError::getDefaultMessage)
+        .orElse("Validation error");
 
+    ErrorResponse error = new ErrorResponse(
+        HttpStatus.BAD_REQUEST.value(),
+        HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        message,
+        request.getRequestURI()
+    );
+
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(
+      HttpMessageNotReadableException ex,
+      HttpServletRequest request
+  ) {
+    ErrorResponse error = new ErrorResponse(
+        HttpStatus.BAD_REQUEST.value(),
+        HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        ex.getMessage(),
+        request.getRequestURI()
+    );
+
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
 
 
 }
