855bb1bc764f0ea2d8f317c663b85e3e45bd1021
melowillian.dev <melowillian.dev@gmail.com>
2026-03-14 08:31:31 -0300

    feature: cria ValidationErrorResponse e adapta GlobalExceptionHandlew  #489

--- apps/api/src/main/java/br/org/apae/api/common/exceptions/handler/GlobalExceptionHandler.java
+++ apps/api/src/main/java/br/org/apae/api/common/exceptions/handler/GlobalExceptionHandler.java
@@ -1,25 +1,54 @@
 package br.org.apae.api.common.exceptions.handler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.org.apae.api.common.exceptions.types.ErrorResponse;
+import br.org.apae.api.common.exceptions.types.ValidationErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+
+    List<ValidationErrorResponse.FieldError> fieldErrors = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(error -> new ValidationErrorResponse.FieldError(
+                    error.getField(),
+                    error.getDefaultMessage()
+            ))
+            .collect(Collectors.toList());
+
+    ValidationErrorResponse errorResponse = new ValidationErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.BAD_REQUEST.getReasonPhrase(),
+            "Erro de validação",
+            request.getRequestURI(),
+            fieldErrors
+    );
+
+    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+  }
+
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, HttpServletRequest request) {
     ErrorResponse errorResponse = new ErrorResponse(
-        HttpStatus.INTERNAL_SERVER_ERROR.value(),
-        HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-        ex.getMessage(),
-        request.getRequestURI());
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+            ex.getMessage(),
+            request.getRequestURI());
     return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
