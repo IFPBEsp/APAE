ee409c4e4b9881f86654a3b4e610f6ee85ec2f5b
filipe.kevyn.ext <FilipeKevyn>
2026-03-24 21:54:30 -0300

    refactor: trocando  as credênciais por variáveis de ambiente

--- apps/api/src/main/java/br/org/apae/api/auth/infrastructure/security/JwtProvider.java
+++ apps/api/src/main/java/br/org/apae/api/auth/infrastructure/security/JwtProvider.java
@@ -22,8 +22,8 @@ public class JwtProvider implements TokenProvider {
   @Value("${app.token.secret}")
   private String secret;
 
-  @Value("app.token,issuer")
-  private static final String ISSUER = "apae-user";
+  @Value("app.token.issuer")
+  private static String ISSUER;
 
   private Instant genExpirationDate() {
     return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.of("-03:00"));
