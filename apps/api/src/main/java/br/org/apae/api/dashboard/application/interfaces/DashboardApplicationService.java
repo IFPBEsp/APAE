28d4ce4bf26d07f57e58838a3f34411c451ea2d7
dariomatias-dev <matiasdario75@gmail.com>
2026-04-12 12:05:45 -0300

    feat[dashboard/service]: cria contrato do dashboard overview
    - Define DashboardApplicationService.
    - Expõe método getOverview para agregação de métricas.

---
+++ apps/api/src/main/java/br/org/apae/api/dashboard/application/interfaces/DashboardApplicationService.java
@@ -0,0 +1,7 @@
+package br.org.apae.api.dashboard.application.interfaces;
+
+import br.org.apae.api.common.dto.dashboard.response.DashboardOverviewResponseDTO;
+
+public interface DashboardApplicationService {
+    DashboardOverviewResponseDTO getOverview();
+}
