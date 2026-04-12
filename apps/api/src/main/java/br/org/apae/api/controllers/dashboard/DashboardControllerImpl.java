1dd425d7342d2009f559bdb266565e4614d0054f
dariomatias-dev <matiasdario75@gmail.com>
2026-04-12 12:06:36 -0300

    feat[dashboard/controller]: implementa endpoint de dashboard overview
    - Expõe DashboardControllerImpl.
    - Integra DashboardApplicationService.
    - Retorna métricas consolidadas do sistema.

---
+++ apps/api/src/main/java/br/org/apae/api/controllers/dashboard/DashboardControllerImpl.java
@@ -0,0 +1,22 @@
+package br.org.apae.api.controllers.dashboard;
+
+import br.org.apae.api.common.dto.dashboard.response.DashboardOverviewResponseDTO;
+import br.org.apae.api.dashboard.application.interfaces.DashboardApplicationService;
+import br.org.apae.api.dashboard.interfaces.controllers.DashboardController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DashboardControllerImpl implements DashboardController {
+
+    private final DashboardApplicationService dashboardService;
+
+    public DashboardControllerImpl(DashboardApplicationService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @Override
+    public ResponseEntity<DashboardOverviewResponseDTO> getOverview() {
+        return ResponseEntity.ok(dashboardService.getOverview());
+    }
+}
