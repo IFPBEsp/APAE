0f7b8f5657cd20a76149554955af45a517944a2b
João <joao.monte@academico.ifpb.edu.br>
2026-05-19 16:29:07 -0300

    NotType-Test: Corrigir testes com status HTTP incorretos no DisorderControllerImplTest

--- apps/api/src/test/java/br/org/apae/api/controllers/disorder/DisorderControllerImplTest.java
+++ apps/api/src/test/java/br/org/apae/api/controllers/disorder/DisorderControllerImplTest.java
@@ -6,6 +6,7 @@ import br.org.apae.api.auth.infrastructure.security.SecurityConfiguration;
 import br.org.apae.api.common.dto.patient.request.disorder.CreateDisorderDTO;
 import br.org.apae.api.common.dto.patient.request.disorder.UpdateDisorderDTO;
 import br.org.apae.api.common.dto.patient.response.disorder.DisorderResponseDTO;
+import br.org.apae.api.common.exceptions.handler.GlobalExceptionHandler;
 import br.org.apae.api.helpers.AuthTestHelper;
 import br.org.apae.api.patient.application.interfaces.DisorderApplicationService;
 import br.org.apae.api.patient.domain.exceptions.DisorderConflictException;
@@ -46,7 +47,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc(addFilters = true)
 @Import({
         SpringDataWebConfiguration.class,
-        SecurityConfiguration.class
+        SecurityConfiguration.class,
+        GlobalExceptionHandler.class
 })
 @Tag("patient")
 @Tag("unit")
