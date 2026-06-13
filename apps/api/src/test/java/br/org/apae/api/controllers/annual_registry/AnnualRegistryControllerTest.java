f3f3988295702296c4952dbe14313bcb21c29fb7
Emanuelle Guedes <manugguedes1701@gmail.com>
2026-06-13 04:53:52 -0300

    test: resolvendo problemas de execucao envi=olvendo testes do sistema

--- apps/api/src/test/java/br/org/apae/api/controllers/annual_registry/AnnualRegistryControllerTest.java
+++ apps/api/src/test/java/br/org/apae/api/controllers/annual_registry/AnnualRegistryControllerTest.java
@@ -1,4 +1,4 @@
-package br.org.apae.api.controllers.annualregistry;
+package br.org.apae.api.controllers.annual_registry;
 
 import br.org.apae.api.auth.application.internal.UserService;
 import br.org.apae.api.auth.infrastructure.security.JwtProvider;
@@ -8,7 +8,6 @@ import br.org.apae.api.common.dto.patient.request.annualregistry.ReplaceAnnualRe
 import br.org.apae.api.common.dto.patient.request.annualregistry.UpdateAnnualRegistryDTO;
 import br.org.apae.api.common.dto.patient.response.annualregistry.AnnualRegistryResponseDTO;
 import br.org.apae.api.common.exceptions.handler.GlobalExceptionHandler;
-import br.org.apae.api.controllers.annual_registry.AnnualRegistryControllerImpl;
 import br.org.apae.api.helpers.AuthTestHelper;
 import br.org.apae.api.patient.application.interfaces.AnnualRegistryApplicationService;
 import br.org.apae.api.patient.domain.exceptions.AnnualRegistryConflictException;
