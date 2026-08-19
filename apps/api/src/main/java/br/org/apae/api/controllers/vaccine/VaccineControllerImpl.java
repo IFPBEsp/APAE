9e51899e9c69f053c8f87fbe8bf5382d2683683d
Mateus23-silva <miranda2k4@gmail.com>
2026-08-19 15:31:26 -0300

    feat[api]: cria endpoints de escrita no VaccineController #878

--- apps/api/src/main/java/br/org/apae/api/controllers/vaccine/VaccineControllerImpl.java
+++ apps/api/src/main/java/br/org/apae/api/controllers/vaccine/VaccineControllerImpl.java
@@ -1,13 +1,21 @@
 package br.org.apae.api.controllers.vaccine;
 
-import br.org.apae.api.patient.application.interfaces.VaccineApplicationService;
-import br.org.apae.api.common.dto.patient.response.vaccine.VaccineResponseDTO;
-import br.org.apae.api.patient.interfaces.controllers.VaccineController;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.org.apae.api.common.dto.patient.request.vaccine.CreateVaccineDTO;
+import br.org.apae.api.common.dto.patient.request.vaccine.UpdateVaccineDTO;
+import br.org.apae.api.common.dto.patient.response.vaccine.VaccineResponseDTO;
+import br.org.apae.api.patient.application.interfaces.VaccineApplicationService;
+import br.org.apae.api.patient.interfaces.controllers.VaccineController;
+import jakarta.validation.Valid;
 
 @RestController
 public class VaccineControllerImpl implements VaccineController {
@@ -35,5 +43,23 @@ public class VaccineControllerImpl implements VaccineController {
     public ResponseEntity<VaccineResponseDTO> findByName(String name) {
         VaccineResponseDTO vaccine = vaccineService.findVaccineByName(name);
         return ResponseEntity.ok(vaccine);
+    }
+
+    @Override
+    public ResponseEntity<VaccineResponseDTO> createVaccine(@Valid @RequestBody CreateVaccineDTO dto) {
+        VaccineResponseDTO created = vaccineService.createVaccine(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @Override
+    public ResponseEntity<VaccineResponseDTO> updateVaccine(@PathVariable UUID id, @Valid @RequestBody UpdateVaccineDTO dto) {
+        VaccineResponseDTO updated = vaccineService.updateVaccine(id, dto);
+        return ResponseEntity.ok(updated);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteVaccine(@PathVariable UUID id) {
+        vaccineService.deleteVaccine(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- apps/api/src/main/java/br/org/apae/api/patient/interfaces/controllers/VaccineController.java
+++ apps/api/src/main/java/br/org/apae/api/patient/interfaces/controllers/VaccineController.java
@@ -1,14 +1,25 @@
 package br.org.apae.api.patient.interfaces.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.org.apae.api.common.dto.patient.request.vaccine.CreateVaccineDTO;
+import br.org.apae.api.common.dto.patient.request.vaccine.UpdateVaccineDTO;
 import br.org.apae.api.common.dto.patient.response.vaccine.VaccineResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.UUID;
 
 @RequestMapping("/vaccines")
 @Tag(name = "Vaccines", description = "Endpoints para consulta de vacinas")
@@ -36,4 +47,13 @@ public interface VaccineController {
         })
         @GetMapping("/search/by-name")
         ResponseEntity<VaccineResponseDTO> findByName(@RequestParam String name);
+        
+        @PostMapping
+        ResponseEntity<VaccineResponseDTO> createVaccine(@jakarta.validation.Valid @RequestBody CreateVaccineDTO dto);
+
+        @PutMapping("/{id}")
+        ResponseEntity<VaccineResponseDTO> updateVaccine(@PathVariable UUID id, @jakarta.validation.Valid @RequestBody UpdateVaccineDTO dto);
+
+        @DeleteMapping("/{id}")
+        ResponseEntity<Void> deleteVaccine(@PathVariable UUID id);
 }
