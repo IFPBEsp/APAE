5c0f950f32d78d0417003c2c52c9b51fef9f8a6b
Wolgrand Araújo Pinheiro <wolgrand.araujo.ext@synchro.com.br>
2026-09-06 17:48:52 -0300

    test

--- apps/api/src/test/java/br/org/apae/api/auth/application/internal/UserServiceTest.java
+++ apps/api/src/test/java/br/org/apae/api/auth/application/internal/UserServiceTest.java
@@ -33,7 +33,7 @@ class UserServiceTest {
   @Test
   void testePropositalGateCI() {
       assertEquals(1, 2, "Teste proposital para validar o gate de CI");
-  }
+  } 
 
   @Test
   void createUserShouldRejectDuplicatedEmail() {
